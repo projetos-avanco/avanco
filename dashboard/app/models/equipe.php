646ea16efc773a3d7688dfe3dad114221add8d4f
Wellington Felix <wellington_bhmg@hotmail.com>
2017-07-28 09:29:26 -0300

    adicinado modelo de equipes para o dashboard de colaboradores

---
+++ dashboard/app/models/equipe.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * define um modelo de equipe
+ */
+function defineArrayComModeloDeEquipe()
+{
+  $equipe = array(
+    'nome' => '',
+    'codigo' => '',
+    'data_entrada' => '',
+    'data_saida' => ''
+  );
+
+  return $equipe;
+}
