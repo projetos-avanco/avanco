951ef4564c323672a1ea1e168090832dd279821d
Wellington Felix <wellington_bhmg@hotmail.com>
2017-07-28 17:02:57 -0300

    alterações no script de inicialização

--- dashboard/init.php
+++ dashboard/init.php
@@ -13,7 +13,3 @@ require_once 'config/database/configuracoes.php';
 
 # importando script com as funções para conexão com a base de dados
 require_once 'database/conexao.php';
-
-$conexao = abre_conexao();
-
-exit(var_dump($conexao));
