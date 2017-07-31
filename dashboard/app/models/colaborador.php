5d91ffe9ae8866045f1920d8ab1be6e459edb653
Wellington Felix <wellington_bhmg@hotmail.com>
2017-07-31 16:31:10 -0300

    alterações no modelo de colaborador

--- dashboard/app/models/colaborador.php
+++ dashboard/app/models/colaborador.php
@@ -6,13 +6,13 @@
 function defineArrayComModeloDeColaborador()
 {
   $colaborador = array(
-    'informacoes_pessoais' => array(
+    'pessoal' => array(
       'id' => 0,
       'nome' => '',
       'sobrenome' => ''
     ),
 
-    'informacoes_atendimento' => array(
+    'atendimento' => array(
       'atendimentos_demandados' => 0,
       'atendimentos_realizados' => 0,
       'atendimentos_perdidos' => 0,
@@ -21,16 +21,33 @@ function defineArrayComModeloDeColaborador()
       'tma' => ''
     ),
 
-    'indicadores' => array(
+    'indices' => array(
       'percentual_avancino' => '',
       'percentual_eficiencia' => '',
       'percentual_questionario_respondido' => ''
     ),
 
     'outros' => array(
-      'quantidade_artigos_infovarejo' => 0,
-      'quantidade_documentos_bc' => 0,
-      'percentual_sla' => ''
+      'infovarejo' => array(
+        'artigos' => array(
+          'data' => '',
+          'nome' => ''
+        ),
+        'quantidade_mes_vigente' => 0,
+        'total_acumulado' => 0
+      ),
+      'base_conhecimento' => array(
+        'documentos' => array(
+          'data' => '',
+          'nome' => ''
+        ),
+        'quantidade_mes_vigente' => 0,
+        'total_acumulado' => 0
+      ),
+      'percentual_sla' => array(
+        'mes_vigente' => '0',
+        'total_acumulado' => '0'
+      )
     )
   );
 
