b503aa418fafc4787e468ceb6dab1838c6df4be8
Wellington Felix <wellington_bhmg@hotmail.com>
2017-08-09 14:03:50 -0300

    adicionado script com o modelo de dashboard

---
+++ dashboard/app/models/dashboard.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * define um array modelo de dashboard
+ */
+function defineArrayModeloDeDashboard()
+{
+  $dashboard = array(
+    'colaborador' => array(
+      'nome'         => '',
+      'sobrenome'    => '',
+      'caminho_foto' => ''
+    ),
+
+    'indicadores_chat' => array(
+      'atendimentos_demandados'            => '',
+      'atendimentos_realizados'            => '',
+      'percentual_perda'                   => '',
+      'percentual_fila_ate_15_minutos'     => '',
+      'tma'                                => '',
+      'percentual_avancino'                => '',
+      'percentual_eficiencia'              => '',
+      'percentual_questionario_respondido' => '',
+    ),
+
+    'titulos_conquistados' => array(
+      'nome'           => array(),
+      'data_premiacao' => array()
+    ),
+
+    'informacoes_gerais' => array(
+      'infovarejo' => array(
+        'quantidade_mes_artigos_infovarejo'   => '',
+        'quantidade_total_artigos_infovarejo' => ''
+      ),
+      'base_conhecimento' => array(
+        'quantidade_mes_documentos_bc'   => '',
+        'quantidade_total_documentos_bc' => ''
+      ),
+      'sla' => array(
+        'percentual_mes_sla'   => '',
+        'percentual_total_sla' => ''
+      )
+    ),
+  );
+
+  return $dashboard;
+}
