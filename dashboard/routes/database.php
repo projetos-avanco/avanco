f3c36ce0d5af1c1de7fc8b33aa6fa69520318ea3
Wellington Felix <wellington_bhmg@hotmail.com>
2017-08-11 10:07:10 -0300

    adicionado scripts com as rotas dos diretórios database, helpers e models

---
+++ dashboard/routes/database.php
@@ -0,0 +1,13 @@
+<?php
+
+# definindo constante com o path do diretório functions
+if (! defined('DIRETORIO_FUNCTIONS'))
+  define('DIRETORIO_FUNCTIONS', ABS_PATH . 'database/functions/');
+
+# definindo constante com o path do diretório data
+if (! defined('DIRETORIO_DATA'))
+  define('DIRETORIO_DATA', ABS_PATH . 'database/functions/profile/data');
+
+# definindo constante com o path do diretório tables
+if (! defined('DIRETORIO_TABLES'))
+  define('DIRETORIO_TABLES', ABS_PATH . 'database/functions/profile/tables');
