270cdb67649e3705fa144a43f3ff105e3715d830
Wellington Felix <wellington_bhmg@hotmail.com>
2017-08-28 09:48:06 -0300

    trabalhando no formulário de login

--- dashboard/public/views/login/form_login.php
+++ dashboard/public/views/login/form_login.php
@@ -2,62 +2,61 @@
 
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <title>Formulario de Login</title>
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>libs/normalize/css/normalize-7.0.0.css">
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>libs/bootstrap-4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  </head>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <body>
+  <title>Formulario de Login</title>
 
+  <link rel="stylesheet" href="<?php echo BASE_URL; ?>libs/normalize/css/normalize-7.0.0.css">
+  <link rel="stylesheet" href="<?php echo BASE_URL; ?>libs/bootstrap-4.0.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/form_login.css">
+</head>
 
+<body>
 
-    <div class="container">
-      <div class="row" style="padding-top: 80px;">
+  <header>
 
-        <div class="col-md-4">
-        </div>
+  </header>
 
-          <div class="col-md-4">
-            <div class="card">
-              <div class="card-header text-center">
-                <h5 >Login</h5>
-              </div>
-                <div class="card-body">
-                  <form action="<?php echo BASE_URL; ?>app/requests/processa_login.php" method="post">
-                    <div class="form-group">
-                      <!--label for="usuario">Usuário</label-->
-                      <input type="text" class="form-control" id="usuario" name="login[usuario]" aria-describedby="userHelp" placeholder="Digite o usuário">
-                      <small id="userHelp" class="form-text text-muted">Seu usuário consiste em nome.sobrenome</small>
-                    </div>
-                    <div class="form-group">
-                      <!--label for="senha">Senha</label-->
-                      <input type="password" class="form-control" id="senha"  name="login[senha]" placeholder="Digite a senha" >
-                    </div>
-                    <div class="form-check">
-                      <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input">
-                        <text class=" text-muted">Lembrar Usuário</text>
-                      </label>
-                    </div>
-                    <button type="submit" id="teste" class="btn btn-lg btn-success btn-block">Entrar</button>
-                  </form>
-                </div>
+  <main>
+    <div class="container"><!-- container -->
+      <div class="row justify-content-sm-center"><!-- linha -->
+        <div class="col-sm-auto text-center"><!-- coluna -->
+          <div class="card"><!-- card -->
+            <div class="card-header">
+              <h5>Login</h5>
             </div>
-          </div>
+            <div class="card-body"><!-- corpo do card -->
+              <form action="<?php echo BASE_URL; ?>app/requests/processa_login.php" method="post"><!-- formulário -->
+                <div class="form-group">
+                  <input type="text" class="form-control" id="usuario" name="login[usuario]" placeholder="Usuário">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" id="senha"  name="login[senha]" placeholder="Senha" >
+                </div>
+                <div class="form-check text-left">
+                  <label for="check" class="form-check-label">
+                    <input type="checkbox" class="form-check-input">
+                    Lembre-me
+                  </label>
+                </div>
+                <button type="submit" class="btn btn-success btn-block">Entrar</button>
+              </form><!-- formulário -->
+            </div><!-- corpo do card -->
+          </div><!-- card -->
+        </div><!-- coluna -->
+      </div><!-- linha -->
+    </div><!-- container -->
+  </main>
 
-          <div class="col-md-4">
-          <div>
+  <footer>
 
-        </div>
-      </div>
+  </footer>
 
-
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-      <script src="<?php echo BASE_URL; ?>libs/bootstrap-4.0.0/js/bootstrap.min.js"></script>
-
-  </body>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+  <script src="<?php echo BASE_URL; ?>libs/bootstrap-4.0.0/js/bootstrap.min.js"></script>
+</body>
 </html>
