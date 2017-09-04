2c4d6f19d35ccfbed84cc5b6e9f97acdf7da48be
Wellington Felix <wellington_bhmg@hotmail.com>
2017-09-04 16:54:00 -0300

    trabalhando no estilo dos gráficos

--- dashboard/database/functions/graphics/conhecimento.php
+++ dashboard/database/functions/graphics/conhecimento.php
@@ -37,75 +37,75 @@ function verificaConhecimentoDoColaborador($conexao, $id)
     # preencho a sessão com o nível de conhecimento de acordo com o id do módulo
     switch ($i) {
       case 1:
-        $_SESSION['graficos']['integral']['materiais'] = (float)$percentual[0];
+        $_SESSION['graficos']['integral']['materiais'] = $percentual[0];
         break;
 
       case 2:
-        $_SESSION['graficos']['integral']['fiscal'] = (float)$percentual[0];
+        $_SESSION['graficos']['integral']['fiscal'] = $percentual[0];
         break;
 
       case 3:
-        $_SESSION['graficos']['integral']['financeiro'] = (float)$percentual[0];
+        $_SESSION['graficos']['integral']['financeiro'] = $percentual[0];
         break;
 
       case 4:
-        $_SESSION['graficos']['integral']['contabil'] = (float)$percentual[0];
+        $_SESSION['graficos']['integral']['contabil'] = $percentual[0];
         break;
 
       case 5:
-        $_SESSION['graficos']['integral']['cotacao'] = (float)$percentual[0];
+        $_SESSION['graficos']['integral']['cotacao'] = $percentual[0];
         break;
 
       case 6:
-        $_SESSION['graficos']['integral']['tnfe'] = (float)$percentual[0];
+        $_SESSION['graficos']['integral']['tnfe'] = $percentual[0];
         break;
 
       case 7:
-        $_SESSION['graficos']['integral']['wms'] = (float)$percentual[0];
+        $_SESSION['graficos']['integral']['wms'] = $percentual[0];
         break;
 
       case 8:
-        $_SESSION['graficos']['frente_de_loja']['frente_windows'] = (float)$percentual[0];
+        $_SESSION['graficos']['frente_de_loja']['frente_windows'] = $percentual[0];
         break;
 
       case 9:
-        $_SESSION['graficos']['frente_de_loja']['frente_linux'] = (float)$percentual[0];
+        $_SESSION['graficos']['frente_de_loja']['frente_linux'] = $percentual[0];
         break;
 
       case 10:
-        $_SESSION['graficos']['frente_de_loja']['supervisor'] = (float)$percentual[0];
+        $_SESSION['graficos']['frente_de_loja']['supervisor'] = $percentual[0];
         break;
 
       case 11:
-        $_SESSION['graficos']['frente_de_loja']['scanntech'] = (float)$percentual[0];
+        $_SESSION['graficos']['frente_de_loja']['scanntech'] = $percentual[0];
         break;
 
       case 12:
-        $_SESSION['graficos']['frente_de_loja']['sitef'] = (float)$percentual[0];
+        $_SESSION['graficos']['frente_de_loja']['sitef'] = $percentual[0];
         break;
 
       case 13:
-        $_SESSION['graficos']['frente_de_loja']['comandas'] = (float)$percentual[0];
+        $_SESSION['graficos']['frente_de_loja']['comandas'] = $percentual[0];
         break;
 
       case 14:
-        $_SESSION['graficos']['gestor']['instalacao'] = (float)$percentual[0];
+        $_SESSION['graficos']['gestor']['instalacao'] = $percentual[0];
         break;
 
       case 15:
-        $_SESSION['graficos']['gestor']['cadastro'] = (float)$percentual[0];
+        $_SESSION['graficos']['gestor']['cadastro'] = $percentual[0];
         break;
 
       case 16:
-        $_SESSION['graficos']['gestor']['movimento'] = (float)$percentual[0];
+        $_SESSION['graficos']['gestor']['movimento'] = $percentual[0];
         break;
 
       case 17:
-        $_SESSION['graficos']['gestor']['contabil'] = (float)$percentual[0];
+        $_SESSION['graficos']['gestor']['contabil'] = $percentual[0];
         break;
 
       case 18:
-        $_SESSION['graficos']['gestor']['fiscal'] = (float)$percentual[0];
+        $_SESSION['graficos']['gestor']['fiscal'] = $percentual[0];
         break;
 
     }
