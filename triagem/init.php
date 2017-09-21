ce65ffc30b4501861cc858d0d6a6184a7fea5b94
Wellington Felix <wellington_bhmg@hotmail.com>
2017-09-21 11:06:51 -0300

    definido configurações para exibição de erros e importando o script de rotas do servidor web

--- triagem/init.php
+++ triagem/init.php
@@ -1,1 +1,9 @@
 <?php
+
+# definindo configurações para exibição de erros
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+error_reporting(E_ALL);
+
+# importando script com as rotas do servidor web
+require_once 'routes/servidor.php';
