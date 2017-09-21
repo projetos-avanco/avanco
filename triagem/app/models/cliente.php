2d9af0e2c1c91d98c0bf616bed1493f37a1546a3
Wellington Felix <wellington_bhmg@hotmail.com>
2017-09-21 13:56:05 -0300

    criado script modelo que contém uma função que retorna um array modelo de cliente

---
+++ triagem/app/models/cliente.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * cria um array modelo de cliente
+ */
+function criaModeloDeCliente()
+{
+  $cliente = array(
+    'nome' => '',
+    'nome_usuario' => '',
+    'cnpj' => '',
+    'conta_contrato' => '',
+    'razao_social' => '',
+    'produto' => '',
+    'modulo' => '',
+    'duvida' => '',
+    'departamento' => ''
+  );
+
+  return $cliente;
+}
