2b19947853f2178da1a7a7f3851069f60db3c516
Wellington Felix <wellington_bhmg@hotmail.com>
2017-09-29 12:06:17 -0300

    adicionado função que aguarda até que um colaborador esteja online no chat

--- triagem/app/helpers/diversas.php
+++ triagem/app/helpers/diversas.php
@@ -18,7 +18,7 @@ function comparaChavesDosArraysInternos($a, $b)
 
 /**
  * elimina do array os colaboradores que possuem menos de 20% de conhecimento sobre o módulo selecionado pelo cliente no portal avanço
- * @param - $array com os colaboradores que estão online
+ * @param - array com os colaboradores que estão online
  * @param - inteiro com a quantidade de colaboradores no array
  */
 function eliminaColaboradoresSemConhecimento($array, $quantidade)
@@ -36,4 +36,37 @@ function eliminaColaboradoresSemConhecimento($array, $quantidade)
   }
 
   return $array;
+}
+
+/**
+ * monta uma URL com os dados do cliente e com o código do departamento para qual o cliente será transferido
+ * @param - array com os dados dos colaboradores que estão online
+ * @param - array com os dados do cliente
+ */
+function montaURL($colaboradores, $cliente)
+{
+  $url = '';
+
+  # montando URL
+  $url =   "index.php/por/chat/startchat/(leaveamessage)/true?prefill%5Busername%5D={$cliente['nome_usuario']}&value_items_admin[0]={$cliente['duvida']}&value_items_admin[1]={$cliente['nome']}&value_items_admin[2]={$cliente['conta_contrato']}&value_items_admin[3]={$cliente['razao_social']}&value_items_admin[4]={$cliente['cnpj']}&portalKey=1505758004&prefill%5Bphone%5D={$colaboradores[0]['departamento']}";
+
+  return $url;
+}
+
+/**
+ * aguarda até que um ou mais colaboradores fiquem online no chat
+ * @param - array com os dados dos colaboradores
+ * @param - objeto com uma conexão aberta
+ */
+function aguardaColaradoresOnline($colaboradores, $db)
+{
+  # chamando a função até que tenha pelo menos um colaborador online
+  while ($colaboradores == NULL OR $colaboradores[0]['id'] == '') {
+
+    # chamando a função que retorna um array com os dados dos colaboradores online
+    $colaboradores = verificaColaboradoresOnlineNoChat($colaboradores, $db);
+
+  }
+
+  return $colaboradores;
 }
