53372fe35691ab4c4264a7c285e9dcc2374921eb
Wellington Felix <wellington_bhmg@hotmail.com>
2017-09-29 10:24:56 -0300

    adicionado função que elimina colaboradores sem conhecimento

--- triagem/app/helpers/diversas.php
+++ triagem/app/helpers/diversas.php
@@ -15,3 +15,25 @@ function comparaChavesDosArraysInternos($a, $b)
 
   return ($a['fila'] < $b['fila']) ? -1 : 1;
 }
+
+/**
+ * elimina do array os colaboradores que possuem menos de 20% de conhecimento sobre o módulo selecionado pelo cliente no portal avanço
+ * @param - $array com os colaboradores que estão online
+ * @param - inteiro com a quantidade de colaboradores no array
+ */
+function eliminaColaboradoresSemConhecimento($array, $quantidade)
+{
+  # eliminando colaboradores que possuem menos de 20% de conhecimento
+  for ($i = 0; $i < $quantidade; $i++) {
+
+    # verificando se o colaborador possue menos do que 20% de conhecimento
+    if ($array[$i]['conhecimento'] < '20.0') {
+
+      unset($array[$i]);
+
+    }
+
+  }
+
+  return $array;
+}
