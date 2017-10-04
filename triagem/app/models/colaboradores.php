c31b3de6acb9a8e79e23615efdbd588a174beaf3
Wellington Felix <wellington_bhmg@hotmail.com>
2017-10-04 10:19:34 -0300

    adicionado mais uma posição no array

--- triagem/app/models/colaboradores.php
+++ triagem/app/models/colaboradores.php
@@ -7,12 +7,13 @@ function criaModeloDeColaboradores()
 {
   $colaboradores = array(
     array(
-      'id'           => '',
-      'nome'         => '',
-      'sobrenome'    => '',      
-      'conhecimento' => '0',
-      'fila'         => '0',
-      'departamento' => '0'
+      'id'              => '',
+      'nome'            => '',
+      'sobrenome'       => '',
+      'conhecimento'    => '0',
+      'fila'            => '0',
+      'id_departamento' => '0',
+      'departamento'    => ''
     )
   );
 
