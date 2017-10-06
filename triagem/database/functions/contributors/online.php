d68d4be17d366dc6e79f6a9aa284cec822efd4b3
Wellington Felix <wellington_bhmg@hotmail.com>
2017-10-06 14:55:07 -0300

    alterado consulta que busca por colaboradores online

--- triagem/database/functions/contributors/online.php
+++ triagem/database/functions/contributors/online.php
@@ -9,23 +9,25 @@ function verificaColaboradoresOnlineNoChat($array, $db)
 {
   $query =
     "SELECT DISTINCT
-    	u.user_id AS id,
-    	s.name AS nome,
-    	s.surname AS sobrenome,
-    	d.id AS id_departamento,
+      u.user_id AS id,
+      s.name AS nome,
+      s.surname AS sobrenome,
+      d.id AS id_departamento,
       d.name AS departamento
     FROM lh_departament AS d
     INNER JOIN lh_userdep AS u
-    	ON u.dep_id = d.id
+      ON u.dep_id = d.id
     INNER JOIN lh_users AS s
-    	ON s.id = u.user_id
-    WHERE (s.id <> 42)
-    	AND (s.id <> 43)
-    	AND (d.disabled = 0)
-    	AND (s.disabled = 0)
-    	AND (u.last_activity > 0)
-    	AND (FROM_UNIXTIME(u.last_activity, '%Y-%m-%d') = CURRENT_DATE())
-    	AND (TIMEDIFF(CURRENT_TIME(), FROM_UNIXTIME(u.last_activity, '%H:%i:%s')) <= '00:05:00')
+      ON s.id = u.user_id
+    WHERE NOT (d.id = 1 OR d.id = 2 OR d.id = 3 OR d.id = 4 OR d.id = 6 OR d.id = 7 OR d.id = 8 OR d.id = 9 OR d.id = 10 OR d.id = 11)
+      AND (s.hide_online = 'false')
+      AND (d.disabled = 0)
+      AND (s.disabled = 0)
+      AND (u.last_activity > 0)
+      AND (s.id <> 42)
+      AND (s.id <> 43)
+      AND (FROM_UNIXTIME(u.last_activity, '%Y-%m-%d') = CURRENT_DATE())
+      AND (TIMEDIFF(CURRENT_TIME(), FROM_UNIXTIME(u.last_activity, '%H:%i:%s')) <= '00:05:00')
     ORDER BY id";
 
   # verificando se a query pode ser executada
