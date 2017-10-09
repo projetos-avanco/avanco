771b15ab67d1460223c5982702cbf586e1e7c9fe
Wellington Felix <wellington_bhmg@hotmail.com>
2017-10-09 12:51:09 -0300

    verificando se o cliente deseja atendimento na tecnologia, no novo erp ou no suporte

--- triagem/app/requests/recebe_cliente.php
+++ triagem/app/requests/recebe_cliente.php
@@ -2,12 +2,10 @@
 
 require '../../init.php';
 
-require ABS_PATH . 'app/models/cliente.php';
 require ABS_PATH . 'app/modules/transfer/tecnologia.php';
-require ABS_PATH . 'app/modules/transfer/suporte_geral.php';
+require ABS_PATH . 'app/modules/transfer/suporte.php';
 require ABS_PATH . 'app/modules/transfer/novo_erp.php';
 require ABS_PATH . 'app/modules/knowledge-base/documentos.php';
-require ABS_PATH . 'app/helpers/requisicoes.php';
 
 # verificando se foi enviado uma requisição via método POST para esse script
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -18,15 +16,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   # verificando se os dados do cliente foram enviados
   if ($confirma) {
 
+    # chamando função que retorna um array modelo para receber os dados do cliente
+    $cliente = criaModeloDeCliente();
+
+    # chamando função que recupera os dados do cliente no array super-global $_POST
+    recuperaDados($cliente, 'POST');
+
+    # chamando função que consulta a dúvida do cliente na base de conhecimento
+    consultaDuvidaNaBaseDeConhecimento();
+
     # verificando se o cliente deseja falar no departamento de tecnologia
     if (isset($_POST['cliente']['tecnologia']) AND ! empty($_POST['cliente']['tecnologia'])) {
 
-      echo 'chamou tecnologia'; exit;
+      # chamando função que consulta informações na base de dados do chat
+      consultaChatTecnologia();
 
     # verificando se o cliente deseja falar no departamento do novo ERP
     } elseif (isset($_POST['cliente']['novo_erp']) AND ! empty($_POST['cliente']['novo_erp'])) {
 
-      echo 'chamou novo ERP'; exit;
+      # chamando função que consulta informações na base de dados do chat
+      consultaChatNovoErp();
 
     # verificando se o cliente deseja falar no departamento de suporte geral
     } else {
@@ -37,17 +46,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       # verificando se os dados da demanda foram enviados
       if ($confirma) {
 
-        # chamando função que retorna um array modelo para receber os dados do cliente
-        $cliente = criaModeloDeCliente();
-
-        # chamando função que recupera os dados do cliente no array super-global $_POST
-        recuperaDados($cliente, 'POST');
-
-        # chamando função que consulta a dúvida do cliente na base de conhecimento
-        consultaDuvidaNaBaseDeConhecimento();
-
         # chamando função que consulta informações na base de dados do chat
-        consultaChat();
+        consultaChatSuporte();
 
       } else {
 
