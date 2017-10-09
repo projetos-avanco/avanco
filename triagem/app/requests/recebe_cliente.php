8e8ecc023aff720d79443b366841d49cfb9e778b
Wellington Felix <wellington_bhmg@hotmail.com>
2017-10-09 11:07:05 -0300

    alterando código para verificar em qual departamento o cliente deseja atendimento, tecnologia, suporte geral ou novo erp

--- triagem/app/requests/recebe_cliente.php
+++ triagem/app/requests/recebe_cliente.php
@@ -3,50 +3,80 @@
 require '../../init.php';
 
 require ABS_PATH . 'app/models/cliente.php';
+require ABS_PATH . 'app/modules/transfer/tecnologia.php';
+require ABS_PATH . 'app/modules/transfer/suporte_geral.php';
+require ABS_PATH . 'app/modules/transfer/novo_erp.php';
 require ABS_PATH . 'app/modules/knowledge-base/documentos.php';
-require ABS_PATH . 'app/modules/transfer/redireciona.php';
 require ABS_PATH . 'app/helpers/requisicoes.php';
 
 # verificando se foi enviado uma requisição via método POST para esse script
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-  $valida = false;
+  # chamando função que verifica se os dados do cliente foram enviados
+  $confirma = confirmaEnvioDosDadosDoCliente();
 
-  # verificando se todos os campos foram enviados
-  if (! empty($_POST['cliente']['nome'])           AND
-      ! empty($_POST['cliente']['nome_usuario'])   AND
-      ! empty($_POST['cliente']['cnpj'])           AND
-      ! empty($_POST['cliente']['conta_contrato']) AND
-      ! empty($_POST['cliente']['razao_social'])   AND
-      ! empty($_POST['cliente']['produto'])        AND
-      ! empty($_POST['cliente']['modulo'])         AND
-      ! empty($_POST['cliente']['duvida']))
-        $valida = true;
+  # verificando se os dados do cliente foram enviados
+  if ($confirma) {
 
-  if ($valida) {
+    # verificando se o cliente deseja falar no departamento de tecnologia
+    if (isset($_POST['cliente']['tecnologia']) AND ! empty($_POST['cliente']['tecnologia'])) {
 
-    # chamando função que retorna um array modelo para receber os dados do cliente
-    $cliente = criaModeloDeCliente();
+      echo 'chamou tecnologia'; exit;
 
-    # chamando função que recupera os dados do cliente no array super-global $_POST
-    recuperaDados($cliente, 'POST');
+    # verificando se o cliente deseja falar no departamento do novo ERP
+    } elseif (isset($_POST['cliente']['novo_erp']) AND ! empty($_POST['cliente']['novo_erp'])) {
 
-    # chamando função que consulta a dúvida do cliente na base de conhecimento
-    consultaDuvidaNaBaseDeConhecimento();
+      echo 'chamou novo ERP'; exit;
 
-    # chamando função que consulta informações na base de dados do chat
-    consultaChat();
+    # verificando se o cliente deseja falar no departamento de suporte geral
+    } else {
+
+      # chamando função que verifica se os dados da demanda foram enviados
+      $confirma = confirmaEnvioDosDadosDaDemanda();
+
+      # verificando se os dados da demanda foram enviados
+      if ($confirma) {
+
+        # chamando função que retorna um array modelo para receber os dados do cliente
+        $cliente = criaModeloDeCliente();
+
+        # chamando função que recupera os dados do cliente no array super-global $_POST
+        recuperaDados($cliente, 'POST');
+
+        # chamando função que consulta a dúvida do cliente na base de conhecimento
+        consultaDuvidaNaBaseDeConhecimento();
+
+        # chamando função que consulta informações na base de dados do chat
+        consultaChat();
+
+      } else {
+
+        $msg = 'Produto, Módulo ou Dúvida não foram enviados!';
+
+        # retornando mensagem para o portal avanço
+        echo json_encode($msg, JSON_UNESCAPED_UNICODE);
+
+        exit;
+      }
+
+    }
 
   } else {
 
-    # chamando a função que grava um log
-    gravaLog('existem campos do formulário do cliente que não foram preenchidos', 'warning');
+    $msg = 'Nome, Nome de Usuário, CNPJ, Conta Contrato ou Razão Social não foram enviados!';
 
+    # retornando mensagem para o portal avanço
+    echo json_encode($msg, JSON_UNESCAPED_UNICODE);
+
+    exit;
   }
 
 } else {
 
-  # chamando a função que grava um log
-  gravaLog('os dados não foram enviados via método POST', 'warning');
+  $msg = 'Os dados do formulário não foram enviados via método POST!';
 
+  # retornando mensagem para o portal avanço
+  echo json_encode($msg, JSON_UNESCAPED_UNICODE);
+
+  exit;
 }
