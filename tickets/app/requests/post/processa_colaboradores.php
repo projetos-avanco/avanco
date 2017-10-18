6e734a64dbe381b9cba6b5f81a21be8f053e64ee
Wellington Felix <wellington_bhmg@hotmail.com>
2017-10-18 11:43:10 -0200

    correção de comentário

--- tickets/app/requests/post/processa_colaboradores.php
+++ tickets/app/requests/post/processa_colaboradores.php
@@ -5,7 +5,7 @@ require '../../modules/screen/colaborador.php';
 # verificando se foi enviado requisição via método POST
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-  # chamando função que consulta os colaboradores existentes no chat e cria as opções para o select da tela de ticket
+  # chamando função responsável por criar as opções com os dados dos colaboradores
   criaOpcoesDeColaboradores();
 
 }
