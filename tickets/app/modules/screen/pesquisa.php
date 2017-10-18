870d9b708b2812b72f3770338a41aca1354fdb26
Wellington Felix <wellington_bhmg@hotmail.com>
2017-10-18 11:44:06 -0200

    adicionado script que é responsável pela pesquisa de cliente

---
+++ tickets/app/modules/screen/pesquisa.php
@@ -0,0 +1,23 @@
+<?php
+
+require '../../../init.php';
+
+require ABS_PATH . 'database/functions/screen/instrucoes.php';
+
+/**
+ * responsável por criar as linhas da tabela dinâmica de clientes
+ * @param - string com o cnpj ou razão social do cliente desejado
+ */
+function criaLinhasParaTabelaDinamicaDeClientes($pesquisa)
+{
+  $db    = abre_conexao();
+  $linhas = '';
+
+  # verificando se está sendo pesquisado um cnpj ou uma razão social ($tipo = true -> cnpj ou $tipo = false -> razão social)
+  $tipo = is_numeric($pesquisa);
+
+  # chamando função que consulta os atendimentos do chat e retorna as linhas da tabela dinâmica de clientes para o formulário de novo ticket
+  $linhas = consultaDadosCadastraisDosClientes($pesquisa, $tipo, $linhas, $db);
+
+  echo $linhas;
+}
