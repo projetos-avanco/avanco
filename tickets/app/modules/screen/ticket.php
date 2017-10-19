50e8fa8b238a31c7d05b97c4cebf8faa10f2d9f5
Wellington Felix <wellington_bhmg@hotmail.com>
2017-10-19 14:26:18 -0200

    adicionado script do módulo responsável por processar os dados do formulário de novo ticket

---
+++ tickets/app/modules/screen/ticket.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * responsável por processar os dados do formulário da tela de novo ticket e grava-los no banco de dados
+ *
+ */
+function processaDadosDoFormularioNovoTicket($dados)
+{
+  require DIRETORIO_FUNCTIONS . 'screen/instrucoes.php';
+
+  # abrindo uma conexão
+  $db = abre_conexao();
+
+  # chamando função que grava o novo ticket no banco de dados
+  insereDadosDoFormularioNovoTicket($dados, $db);
+
+  # redirecionando usuário para a tela principal
+  header('Location: ' . BASE_URL . 'public/views/screen/');
+}
