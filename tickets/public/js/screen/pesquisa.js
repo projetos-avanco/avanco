55f16c1459d31c07f4d2390b4a7a98398d6460b9
Wellington Felix <wellington_bhmg@hotmail.com>
2017-10-20 09:47:15 -0200

    paginando a tabela de clientes com o dataTable

--- tickets/public/js/screen/pesquisa.js
+++ tickets/public/js/screen/pesquisa.js
@@ -12,24 +12,56 @@ $('document').ready(function() {
         type: 'post',
         url: BASE_URL + 'app/requests/post/processa_pesquisa.php?pesquisa=' + pesquisa,
         dataType: 'html',
-        success: function(linhas)
+        success: function(tabela)
         {
-          if (linhas === 'erro') {
+          if (tabela === 'erro') {
 
             alert('Ops! Houve um erro durante a execução da consulta de pesquisa.');
 
           } else {
 
-            $('#linhas').html(linhas);
+            $('#bloco').html(tabela);
+
+            // paginando a tabela
+            $('#tabela').dataTable({
+               "oLanguage" : {
+                 "sEmptyTable": "Nenhum registro encontrado",
+                 "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+                 "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+                 "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+                 "sInfoPostFix": "",
+                 "sInfoThousands": ".",
+                 "sLengthMenu": "_MENU_ Contratos exibidos por página",
+                 "sLoadingRecords": "Carregando...",
+                 "sProcessing": "Processando...",
+                 "sZeroRecords": "Nenhum registro encontrado",
+                 "sSearch": "Pesquisar",
+                 "oPaginate": {
+                   "sNext": "Próximo",
+                   "sPrevious": "Anterior",
+                   "sFirst": "Primeiro",
+                   "sLast": "Último"
+                 },
+                 "oAria": {
+                   "sSortAscending": ": Ordenar colunas de forma ascendente",
+                   "sSortDescending": ": Ordenar colunas de forma descendente"
+                 }
+               },
+               "bFilter": false //removendo a pesquisa
+            });
 
           }
 
         },
-        error: function(linhas)
+        error: function(tabela)
         {
-          alert(linhas);
+          alert(tabela);
         }
       });
+
+    } else {
+
+      $('#bloco').empty(); //limpando o bloco da tabela se a pesquisa for vazia
 
     }
 
