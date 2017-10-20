13439594ad7fea7a3fedea90529716a29e353bce
Wellington Felix <wellington_bhmg@hotmail.com>
2017-10-20 12:51:59 -0200

    adicionado os scripts com os modelos de login e usuário

---
+++ capa/app/models/users/usuario.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * cria um array modelo para receber os dados do usuário
+ */
+function criaModeloDeUsuario()
+{
+  $usuario = array(
+    'id'        => 0,
+    'nome'      => '',
+    'sobrenome' => '',
+    'email'     => '',
+    'nivel'     => ''
+  );
+
+  return $usuario;
+}
