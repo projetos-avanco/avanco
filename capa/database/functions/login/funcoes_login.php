27bb6ee221928e10cd2b3ad2ff32175a65808f33
Wellington Felix <wellington_bhmg@hotmail.com>
2017-10-23 08:59:23 -0200

    refatorando código e adiconado função que consulta se o usuário possui id cadastrado no chat

--- capa/database/functions/login/funcoes_login.php
+++ capa/database/functions/login/funcoes_login.php
@@ -1,17 +1,46 @@
 <?php
 
 /**
+ * consulta se o usuário possui id cadastrado no chat
+ * @param - array com com os dados do usuário
+ * @param - objeto com uma conexão aberta
+ */
+function consultaIdDoUsuarioNoChat($usuario, $db)
+{
+  $query =
+    "SELECT
+    	id
+    FROM lh_users
+    WHERE username = '{$usuario['usuario']}';";
+
+  if ($resultado = $db->query($query)) {
+
+    if ($resultado->num_rows > 0) {
+
+      $registro = $resultado->fetch_array();
+
+      $usuario['id'] = $registro['id'];
+
+    }
+
+  }
+
+  return $usuario;
+}
+
+/**
  * verifica na base de dados se email e senha repassados estão corretos
- * @param - uma conexão aberta
  * @param - array com email, senha e senha criptografada
+ * @param - array com o modelo de dados de usuário
+ * @param - uma conexão aberta
  */
 function consultaDadosDoUsuario($login, $usuario, $db)
 {
   $query =
     "SELECT
-      id,
       nome,
       sobrenome,
+      usuario,
       email,
       nivel
     FROM av_usuarios_login
@@ -28,13 +57,16 @@ function consultaDadosDoUsuario($login, $usuario, $db)
       # recuperando dados do usuário
       while ($registro = $resultado->fetch_array(MYSQLI_ASSOC)) {
 
-        $usuario['id']        = $registro['id'];
         $usuario['nome']      = $registro['nome'];
         $usuario['sobrenome'] = $registro['sobrenome'];
+        $usuario['usuario']   = $registro['usuario'];
         $usuario['email']     = $registro['email'];
         $usuario['nivel']     = $registro['nivel'];
 
       }
+
+      # chamando função que consulta se o usuário possui id cadastrado no chat
+      $usuario = consultaIdDoUsuarioNoChat($usuario, $db);
 
       # criando variáveis de sessão com os dados do usuário
       $_SESSION['usuario']['id']        = $usuario['id'];
@@ -42,8 +74,9 @@ function consultaDadosDoUsuario($login, $usuario, $db)
       $_SESSION['usuario']['sobrenome'] = $usuario['sobrenome'];
       $_SESSION['usuario']['email']     = $usuario['email'];
       $_SESSION['usuario']['nivel']     = $usuario['nivel'];
+      $_SESSION['usuario']['usuario']   = $usuario['usuario'];
       $_SESSION['usuario']['logado']    = true;
-
+      
       return true;
 
     } else {
