5c188d97e0855b269f0eb07416ea997298bb7f55
Wellington Felix <wellington_bhmg@hotmail.com>
2017-11-20 15:19:43 -0200

    aumentando o tempo de duração da sessão

--- capa/init.php
+++ capa/init.php
@@ -1,6 +1,7 @@
 <?php
 
 # definindo configurações para exibição de erros
+ini_set('session.gc_maxlifetime', 200000);
 ini_set('display_errors', 1);
 ini_set('log_errors', 1);
 error_reporting(E_ALL);
