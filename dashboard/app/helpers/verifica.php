e3085eb9ac0d2df1be9bcc93fea9f789933231ba
Wellington Felix <wellington_bhmg@hotmail.com>
2017-11-29 10:51:33 -0200

    redirecionando usuário para a página de login do novo capa.

--- dashboard/app/helpers/verifica.php
+++ dashboard/app/helpers/verifica.php
@@ -3,7 +3,12 @@
 # verificando se o usuário não está logado
 if (! verificaUsuarioLogado()) {
 
+  # criando mensagem de aviso na sessão
+  $_SESSION['mensagens']['mensagem'] = '<p class="text-center"><strong>Ops!</strong> Faça o login para acessar o sistema.</p>';
+  $_SESSION['mensagens']['tipo']     = 'warning';
+  $_SESSION['mensagens']['exibe']    = true;
+
   # redirecionando
-  header('Location: ' . BASE_URL . 'public/views/login/form_login.php');
+  header('Location: ' . BASE_URL . '../capa/public/views/login/form_login.php');
 
 }
