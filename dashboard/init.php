68a6345ee52176b561e6e210c80c7391a39832fa
Wellington Felix <wellington_bhmg@hotmail.com>
2017-11-29 10:54:40 -0200

    aumentando o tempo de sessão.

--- dashboard/init.php
+++ dashboard/init.php
@@ -1,11 +1,12 @@
 <?php
 
 # definindo configurações para exibição de erros
-ini_set('display_errors', 0);
-ini_set('log_errors', 0);
+ini_set('session.gc_maxlifetime', 200000);
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
 error_reporting(E_ALL);
 
-# abrindo sessão
+# iniciando sessão
 session_start();
 
 # importando script com as rotas do servidor web
