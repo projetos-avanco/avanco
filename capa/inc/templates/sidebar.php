53700b6717895b37be8ce1d7f76ca927fa10a82b
Wellington Felix <wellington_bhmg@hotmail.com>
2017-11-29 10:50:58 -0200

    alterado a disposição das opções do menu lateral e adicionado nova opção.

--- capa/inc/templates/sidebar.php
+++ capa/inc/templates/sidebar.php
@@ -2,6 +2,32 @@
   <div id="sidebar-wrapper"><!-- sidebar -->
     <ul class="nav sidebar-nav">
       <li class="bordermenu">
+        <a href="#" class="accordion-heading" data-toggle="collapse" data-target="#submenu3">
+          <span class="nav-header-primary">
+            <i class="fa fa-id-card-o" aria-hidden="true"></i>  Dashboard
+            <span class="pull-right">
+                <i id="setinha" class="fa fa-caret-down" aria-hidden="true"></i>
+            </span>
+          </span>
+        </a>
+        <ul class="nav collapse"  id="submenu3">
+        <?php if (isset($_SESSION['usuario']['nivel']) AND $_SESSION['usuario']['nivel'] == 1) : ?>
+          <li>
+            <a href="<?php echo BASE_URL; ?>../dashboard/public/views/profile/colaborador.php" target="_blank">
+              <p>Colaborador<p>
+            </a>
+          </li>
+        <?php elseif(isset($_SESSION['usuario']['nivel']) AND $_SESSION['usuario']['nivel'] == 2) : ?>
+          <li>
+            <a href="<?php echo BASE_URL; ?>../dashboard/public/views/profile/administrador.php" target="_blank">
+              <p>Administrador<p>
+            </a>
+          </li>
+        <?php endif; ?>
+        </ul>
+      </li>
+
+      <li class="bordermenu">
         <a href="#" class="accordion-heading" data-toggle="collapse" data-target="#submenu1">
           <span class="nav-header-primary">
             <i class="fa fa-tags" aria-hidden="true"></i>  Tickets
@@ -15,24 +41,30 @@
           <li><a href="<?php echo BASE_URL; ?>../capa/public/views/tickets/consulta_tickets.php"><p>Consultar<p></a></li>
         </ul>
       </li>
-      <li class="bordermenu">
-        <a href="<?php echo BASE_URL; ?>../dashboard/public/views/login/form_login.php"><i class="fa fa-address-card" aria-hidden="true"></i>   Dashboard</a>
-      </li>
+
       <li class="bordermenu">
         <a href="#" class="accordion-heading" data-toggle="collapse" data-target="#submenu2">
           <span class="nav-header-primary">
-            <i class="fa fa-pie-chart" aria-hidden="true"></i>  Painéis
+            <i class="fa fa-television" aria-hidden="true"></i>  Painéis
             <span class="pull-right">
                 <i id="setinha" class="fa fa-caret-down" aria-hidden="true"></i>
             </span>
           </span>
         </a>
         <ul class="nav collapse"  id="submenu2">
-          <li><a href="<?php echo BASE_URL; ?>../capa/public/views/panels/colaboradores_logados.php"><p>Logados<p></a></li>
+          <li>
+            <a href="<?php echo BASE_URL; ?>../capa/public/views/panels/colaboradores_logados.php">
+              <p>Logados<p>
+            </a>
+          </li>
         </ul>
+      </li>
+      
+      <li class="bordermenu">
+        <a href="#"><i class="fa fa-bar-chart" aria-hidden="true"></i>   Relatórios</a>
       </li>
     </ul>
   </div><!-- sidebar -->
 
   <div id="page-content-wrapper"><!-- conteúdo da página -->
-    <div class="container-fluid"><!-- container -->      
+    <div class="container-fluid"><!-- container -->
