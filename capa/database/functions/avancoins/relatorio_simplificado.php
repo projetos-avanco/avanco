4919c79186ee56c8eb78c56abcf19c611941fe87
Wellington Felix <wellington_bhmg@hotmail.com>
2017-12-29 17:03:57 -0200

    criando script que gera o relatório simplificado de avancoins.

---
+++ capa/database/functions/avancoins/relatorio_simplificado.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * gera um extrato avancoins simples
+ * @param - objeto com uma conexão aberta
+ * @param - array com os dados do formulário de extrato avancoins
+ * @param - array que irá receber as ações diárias
+ * @param - array que irá receber as ações mensais
+ * @param - array que irá receber as ações esporádicas
+ */
+function geraExtratoAvancoinsSimples($db, $form, $acoesDiarias, $acoesMensais, $acoesEsporadicas)
+{
+
+}
