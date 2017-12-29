79e490da22a06c32f4e3eeea3bd88ddd89cd3bcb
Wellington Felix <wellington_bhmg@hotmail.com>
2017-12-29 16:57:23 -0200

    criando funções que definem o modelo de sessão e gravam na sessão o módulo avancoins.

--- capa/app/models/sessao.php
+++ capa/app/models/sessao.php
@@ -6,9 +6,39 @@
 function criaModeloDeSessaoParaMensagens()
 {
   $_SESSION['mensagens'] = array(
+
     'mensagem' => '',
-    'tipo'     => '',
-    'exibe'    => false
+    'tipo' => '',
+    'exibe' => false
+
   );
 
+}
+
+/*
+ * cria um array modelo para receber os dados do módulo avancoins
+ */
+function criaModeloDeSessaoParaAvancoins()
+{
+  # verificando se existem dados na sessão do módulo avnacoins
+  if (isset($_SESSION['avancoins'])) {
+
+    unset($_SESSION['avancoins']);
+
+  }
+
+  $_SESSION['avancoins'] = array('extrato' => '');
+
+}
+
+/**
+ * grava os dados do módulo avancoins na sessão
+ * @param - array com as ações diárias do colaborador
+ * @param - array com as ações mensais do colaborador
+ * @param - array com as ações esporádicas do colaborador
+ * @param - array com os valores totais das ações do colaborador
+ */
+function gravaModeloDeSessaoAvancoins($tabela)
+{
+  $_SESSION['avancoins']['extrato'] = $tabela;
 }
