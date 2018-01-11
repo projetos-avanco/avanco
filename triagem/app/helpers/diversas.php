7ad8c70023de0b71abdbf9c7ec4a1be29f229073
Wellington Felix <wellington_bhmg@hotmail.com>
2018-01-11 14:32:49 -0200

    resolvendo problema de caracteres especiais e enviando o número de telefone do contato na URL do chat.

--- triagem/app/helpers/diversas.php
+++ triagem/app/helpers/diversas.php
@@ -50,10 +50,10 @@ function redirecionaClienteParaDepartamentoTecnologia($cliente)
     "index.php/por/chat/startchat/(leaveamessage)/true?prefill%5Busername%5D={$cliente['razao_social']}&value_items_admin[0]={$cliente['duvida']}&value_items_admin[1]={$cliente['nome_usuario']}&value_items_admin[2]={$cliente['conta_contrato']}&value_items_admin[3]={$cliente['razao_social']}&value_items_admin[4]={$cliente['cnpj']}&value_items_admin[5]=0&nome_departamento=Tecnologia&codigo_ticket=0&novo_erp={$cliente['novo_erp']}&prefill%5Bphone%5D=2&value_items_admin[6]={$cliente['telefone']}";
 
   # redirecionando cliente para o colaborador no chat teste
-  echo json_encode(['url' => 'http://192.168.0.47:9999/' . $url]);
+  echo json_encode(['url' => 'http://192.168.0.47:9999/' . $url], JSON_UNESCAPED_UNICODE);
 
   # redirecionando cliente para o colaborador no chat produção
-  #echo json_encode(['url' => 'https://chat.avancoinfo.net/' . $url]);
+  #echo json_encode(['url' => 'https://chat.avancoinfo.net/' . $url], JSON_UNESCAPED_UNICODE);
 
   exit;
 }
@@ -77,15 +77,15 @@ function redirecionaClienteParaDepartamento($colaboradores, $cliente)
 
     # montando URL para atendimentos com agendamento
     $url =
-      "index.php/por/chat/startchat/(leaveamessage)/true?prefill%5Busername%5D={$cliente['razao_social']}&value_items_admin[0]={$cliente['duvida']}&value_items_admin[1]={$cliente['nome']}&value_items_admin[2]={$cliente['conta_contrato']}&value_items_admin[3]={$cliente['razao_social']}&value_items_admin[4]={$cliente['cnpj']}&value_items_admin[5]={$cliente['produto']}&nome_departamento={$colaboradores[0]['departamento']}&codigo_ticket={$cliente['ticket']}&novo_erp={$cliente['novo_erp']}&prefill%5Bphone%5D={$colaboradores[0]['id_departamento']}";
+      "index.php/por/chat/startchat/(leaveamessage)/true?prefill%5Busername%5D={$cliente['razao_social']}&value_items_admin[0]={$cliente['duvida']}&value_items_admin[1]={$cliente['nome']}&value_items_admin[2]={$cliente['conta_contrato']}&value_items_admin[3]={$cliente['razao_social']}&value_items_admin[4]={$cliente['cnpj']}&value_items_admin[5]={$cliente['produto']}&nome_departamento={$colaboradores[0]['departamento']}&codigo_ticket={$cliente['ticket']}&novo_erp={$cliente['novo_erp']}&prefill%5Bphone%5D={$colaboradores[0]['id_departamento']}&value_items_admin[6]={$cliente['telefone']}";
 
   }
 
   # redirecionando cliente para o colaborador no chat teste
-  echo json_encode(['url' => 'http://192.168.0.47:9999/' . $url]);
+  echo json_encode(['url' => 'http://192.168.0.47:9999/' . $url], JSON_UNESCAPED_UNICODE);
 
   # redirecionando cliente para o colaborador no chat produção
-  #echo json_encode(['url' => 'https://chat.avancoinfo.net/' . $url]);
+  #echo json_encode(['url' => 'https://chat.avancoinfo.net/' . $url], JSON_UNESCAPED_UNICODE);
 
   exit;
 }
