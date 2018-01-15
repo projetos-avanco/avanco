c3ff8aeb2b3bf683089dd51bcf82b400ae41c4c9
Wellington Felix <wellington_bhmg@hotmail.com>
2018-01-15 10:56:02 -0200

    corrigindo caminho de importação da folha de estilo da página.

--- capa/public/views/panels/ramais.php
+++ capa/public/views/panels/ramais.php
@@ -39,7 +39,7 @@
   <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/home.css">
   <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/sidebar.css">
   <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/navbar.css">  
-  <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/users/tabela_ramais.css">  
+  <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/panels/tabela_ramais.css">  
 </head>
 
 <body>
