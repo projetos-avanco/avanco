ba4d529a8d65d34ac0e0b4962779d1cfe7e007ed
Wellington Felix <wellington_bhmg@hotmail.com>
2018-01-22 10:20:21 -0200

    adicionado função que verifica o prazo do agendamento do ticket.

--- triagem/app/modules/transfer/ticket.php
+++ triagem/app/modules/transfer/ticket.php
@@ -3,13 +3,72 @@
 header('Content-Type: text/html; charset=UTF-8');
 
 /**
+ * responsável por verificar se o prazo de agendamento do ticket já passou
+ * @param - array com o número do ticket
+ * @param - array com o modelo de dados de colaboradores
+ */
+function verificaPrazoDoAgendamentoDoTicket($cliente, $colaboradores)
+{
+  require DIRETORIO_FUNCTIONS . 'tickets/funcoes_tickets.php';
+
+  # chamando função que retorna uma conexão com a base de dados
+  $db = abre_conexao();
+  
+  $agendado = consultaPrazoDoAgendamentoDoTicket($cliente['ticket'], $db);
+  
+  # verificando se a data agendada do ticket já passou
+  if ($agendado['data_atual'] > $agendado['data_agendada']) {
+    
+    # chamando função que invalida um ticket
+    invalidaTicketForaDoPrazo($cliente['ticket'], $db);
+
+    $msg = 'Ticket com o Prazo de Agendamento Vencido!';
+
+    #retornando mensagem para o portal avanço
+    echo json_encode($msg, JSON_UNESCAPED_UNICODE);
+
+    exit;
+
+  # verificando se o cliente está chamando na data agendada
+  } else if ($agendado['data_atual'] == $agendado['data_agendada']) {
+
+    # verificando se existe uma diferença maior que 30 minutos no horário que o cliente chamou
+    if ($agendado['diferenca'] > '00:30:00') {
+
+      # chamando função que invalida um ticket
+      invalidaTicketForaDoPrazo($cliente['ticket'], $db);
+
+      $msg = 'Ticket com o Prazo de Agendamento Vencido!';
+
+      #retornando mensagem para o portal avanço
+      echo json_encode($msg, JSON_UNESCAPED_UNICODE);
+
+      exit;
+    
+    # verificando se existe uma diferença menor que 30 minutos no horário que o cliente chamou
+    } else {
+
+      # chamando função responsável por redirecionar o cliente para o colaborador agendado
+      redirecionaParaColaboradorResponsavel($cliente, $colaboradores);
+
+    }
+  
+  # verificando se o cliente está chamando antes da data agendada
+  } else {
+
+      # chamando função responsável por redirecionar o cliente para o colaborador agendado
+      redirecionaParaColaboradorResponsavel($cliente, $colaboradores);
+
+  }
+
+}
+
+/**
  * responsável por verificar se o colaborador está online e redirecionar o cliente para ele
  * @param - array com o número do ticket
  */
 function redirecionaParaColaboradorResponsavel($cliente, $colaboradores)
 {
-  require DIRETORIO_FUNCTIONS . 'tickets/funcoes_tickets.php';
-
   # chamando função que retorna uma conexão com a base de dados
   $db = abre_conexao();
 
