59523cecea2539597a07cd36b95e7e963f00a4e3
Wellington Felix <wellington_bhmg@hotmail.com>
2018-01-29 14:46:53 -0200

    adicionando script js que realiza a compra do produto escolhido.

---
+++ dashboard/public/js/store/compra.js
@@ -0,0 +1,107 @@
+$('document').ready(function() {
+
+  $('button').click(function() {
+        
+    var idProduto   = '';    
+    var descricao   = '';    
+    var confirmacao = false;
+    
+    idProduto = $(this).val(); // recuperando id do produto clicado
+
+    switch (idProduto) {
+
+      case '14':
+        descricao = 'Açai 300ml';
+          break;
+      
+      case '13':
+        descricao = 'Açai 500ml';
+          break;
+
+      case '12':
+        descricao = 'Manicure';
+          break;
+
+      case '11':
+        descricao = 'Ventilador de Mesa';
+          break;
+
+      case '10':
+        descricao = 'PenDrive 64GB';
+          break;
+
+      case '9':
+        descricao = 'Par de Ingressos para o Cinema';
+          break;
+
+      case '8':
+        descricao = 'Rodízio de Pizza com Acompanhante';
+          break;
+
+      case '7':
+        descricao = 'Garrafa de Cachaça Vale Verde';
+          break;
+
+      case '6':
+        descricao = '1 Dia de Folga';
+          break;
+
+      case '5':
+        descricao = 'Garrafa de Jack Daniels';
+          break;
+
+      case '4':
+        descricao = 'Garrafa de Absolut';
+          break;
+
+      case '3':
+        descricao = 'Rodízio Baby Beef com Acompanhante';
+          break;
+
+      case '2':
+        descricao = 'Home Theater';
+          break;
+
+      case '1':
+        descricao = '1 Semana de Folga';
+          break;
+
+    }
+
+    confirmacao = confirm('Confirma a compra do produto ' + descricao + '?'); // aguardando confirmação da compra do produto clicado
+    
+    if (confirmacao) {
+      
+      var idColaborador = '';      
+      var url           = '';
+
+      idColaborador = $('#colaborador').val(); // recuperando id do colaborador que está logado na loja
+      
+      url = '../../../app/requests/ajax/compra_produtos.php'; // path do script que realiza a compra do produto
+
+      $.ajax({
+        type: 'get',
+        url: url + '?idcolaborador=' + idColaborador + '&idproduto=' + idProduto,
+        dataType: 'json',
+        success: function(resposta) {
+
+          alert(resposta);
+          
+          window.location.reload(true); // atualizando página
+
+        },
+        error: function(resposta) {
+          
+          alert('Erro: ' + resposta);
+
+          window.location.reload(true); // atualizando página
+          
+        }
+
+      });
+      
+    }
+
+  });
+
+});
