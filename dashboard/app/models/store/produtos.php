e9b2b8971e32e8176f28a921f042ced9551fd5ed
Wellington Felix <wellington_bhmg@hotmail.com>
2018-01-29 14:52:30 -0200

    adicionando script php com os modelos de produtos.

---
+++ dashboard/app/models/store/produtos.php
@@ -0,0 +1,32 @@
+<?php 
+
+/*
+ * cria um array modelo para receber os dados dos produtos da loja avanção
+ */
+function criaModeloDeProdutos()
+{
+  $produtos = array();
+
+  return $produtos;
+}
+
+
+/*
+ * cria um array modelo para receber os dados da compra
+ */
+function criaArrayModeloDeCompra()
+{
+  $compra = array(
+
+    'id'             => '',
+    'id_colaborador' => '',
+    'id_produto'     => '',
+    'data_compra'    => '',
+    'horario_compra' => '',
+    'envio_email'    => ''
+    
+  );
+
+  return $compra;
+  
+}
