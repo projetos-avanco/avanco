595ce3aaa674db1e8a9fe2263e8ca99b1295db5c
Wellington Felix <wellington_bhmg@hotmail.com>
2018-01-29 14:54:51 -0200

    adicionando script php com a função que envia e-mails com a confirmação das compras na loja avanção e adicionando biblioteca para envio de e-mails.

---
+++ dashboard/app/helpers/store/email.php
@@ -0,0 +1,79 @@
+<?php 
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require_once '../../../libs/PHPMailer/src/PHPMailer.php';
+require_once '../../../libs/PHPMailer/src/SMTP.php';
+
+/**
+ * envia email de confirmação de compra na loja avanção
+ * @param - string com a descrição do produto
+ * @param - string com o nome do colaborador
+ * @param - array com os dados da compra
+ */
+function enviaEmailDeCompraNaLoja($produto, $colaborador, $compra)
+{    
+  require DIRETORIO_HELPERS   . 'data.php';
+
+  # chamando função que formata a data para o formato dd/mm/aaaa
+  $compra['data_compra'] = formataDataParaExibir($compra['data_compra']);
+
+  # escrevendo a mensagem para o corpo do e-mail
+  $mensagem = htmlentities(
+      "Foi solicitado a compra do produto $produto, 
+       na data {$compra['data_compra']}, 
+       no horário {$compra['horario_compra']} pelo colaborador(a) $colaborador");
+
+  $email = new PHPMailer(true);    
+  
+  # chamando função que formata a data para o formato aaaa-mm-dd
+  $compra['data_compra'] = formataUnicaDataParaMysql($compra['data_compra']);
+
+  try {
+
+    # configurações de servidor 
+    #$email->SMTPDebug  = 2;                                 
+    $email->isSMTP();                                      
+    $email->Host       = 'email-ssl.com.br';  
+    $email->SMTPAuth   = true;                               
+    $email->Username   = 'wellington.felix@avancoinfo.com.br';                 
+    $email->Password   = 'Avanco123';                           
+    $email->SMTPSecure = 'ssl';                            
+    $email->Port       = 465;                                    
+
+    # destinatários 
+    $email->setFrom('wellington.felix@avancoinfo.com.br', 'Wellington Felix');
+    $email->addAddress('badaro@avancoinfo.com.br', 'Adilson Badaro');     
+    $email->addAddress('bruno@avancoinfo.com.br', 'Bruno Cesar');               
+    #$email->addReplyTo('info@example.com', 'Information');
+    $email->addCC('wellington.felix@avancoinfo.com.br');
+    $email->addCC('lucas.aguiar@avancoinfo.com.br');
+    #$email->addBCC('bcc@example.com');
+
+    # anexos
+    #$email->addAttachment('/var/tmp/file.tar.gz');         
+    #$email->addAttachment('/tmp/image.jpg', 'new.jpg');    
+
+    # conteúdo
+    $email->isHTML(true);                                  
+    $email->Subject = 'Compra de Produto na Loja';
+    $email->Body    = $mensagem;
+    #$email->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $email->send();
+    
+    # confirmando o envio do e-mail
+    $compra['envio_email'] = true;
+
+  } catch (Exception $e) {
+    
+    # confirmando o não envio do e-mail
+    $compra['envio_email'] = false;
+
+  }
+
+  return $compra;
+
+}
