9d26ed0bd24ac0e981a91f708ee984e0545ba54b
Wellington Felix <wellington_bhmg@hotmail.com>
2018-01-31 16:07:53 -0200

    corrigindo comentário.

--- tickets/app/requests/post/recebe_requisicao_portal.php
+++ tickets/app/requests/post/recebe_requisicao_portal.php
@@ -6,12 +6,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   require '../../modules/portal/requisicoes_portal.php';
 
   # recuperando o código de conta contrato
-  $conta_contrato = isset($_POST['conta_contrato']) ? $_POST['conta_contrato'] : NULL;
+  $conta_contrato = isset($_POST['conta_contrato']) ? $_POST['conta_contrato'] : null;  
 
   # verificando se a conta contrato foi enviada
   if (! empty($conta_contrato)) {
 
-    # chamando função responsável por retornar os tickets válidos referente ao código de conta contrato recebido
+    # chamando função responsável por retornar os tickets referente ao código de conta contrato recebido
     retornaTicketsValidados($conta_contrato);
 
   } else {
