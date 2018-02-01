9293ef222523a405e1f33ffab7544d2d3161344d
Wellington Felix <wellington_bhmg@hotmail.com>
2018-02-01 14:16:18 -0200

    script que será usado no cron para atualizar as carteiras de todos os colaboradores.

--- dashboard/cron/atualiza_carteiras.php
+++ dashboard/cron/atualiza_carteiras.php
@@ -6,14 +6,17 @@ require DIRETORIO_MODULES  . 'avancoins/avancoins.php';
 
 $db = abre_conexao();
 
+# verificando se a conexão com a base de dados foi realizada com sucesso
 if ($db) {
 
   $query = "SELECT id FROM av_avancoins_carteiras ORDER BY id;";
 
+  # verificando se a consulta pode ser executada
   if ($resultado = $db->query($query)) {
 
     $ids = array();
 
+    # recuperando todos os ids dos colaboradores que recebem avancoins
     while ($registro = $resultado->fetch_assoc()) {
 
       $ids[] = $registro['id'];
@@ -22,6 +25,7 @@ if ($db) {
 
   }
 
+  # atualizando a carteira de todos os colaboradores que recebem avancoins
   foreach ($ids as $id) {
 
     # chamando função responsável por atualizar as ações diárias do colaborador no período atual
@@ -34,5 +38,7 @@ if ($db) {
     atualizaCarteira($id);
 
   }
+
+  fecha_conexao($db);
 
 }
