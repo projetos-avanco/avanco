b2eb7eb5cb84e4dd2b7700b4d4c60ea65a4bf18a
Wellington Felix <wellington_bhmg@hotmail.com>
2018-02-01 14:16:48 -0200

    requisitando script init.php para se conectar ao banco de dados.

--- tickets/cron/invalida_tickets.php
+++ tickets/cron/invalida_tickets.php
@@ -1,28 +1,11 @@
 <?php 
 
-# definindo constante com o endereço do host da base de dados
-if (! defined('DB_HOST'))
-  define('DB_HOST', '6.6.6.2');
+require '../init.php';
 
-# definindo constante com o nome do usuário do base de dados
-if (! defined('DB_USER'))
-  define('DB_USER', 'root');
-
-# definindo constante com a senha do usuário da base de dados
-if (! defined('DB_PASSWORD'))
-  define('DB_PASSWORD', 'super');
-
-# definindo constante o nome da base de dados
-if (! defined('DB_NAME'))
-  define('DB_NAME', 'chat_avanco');
-
-# abrindo conexão com a base de dados
-$conexao = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-$conexao->set_charset('utf8');
+$db = abre_conexao();
 
 # verificando se a conexão com a base de dados foi realizada com sucesso
-if ($conexao) {
+if ($db) {
 
   $query =
   "SELECT
@@ -32,7 +15,7 @@ if ($conexao) {
     AND (DATE_FORMAT(agendado, '%Y-%m-%d') < CURRENT_DATE());"; 
   
   # verificando se a consulta pode ser executada
-  if ($resultado = $conexao->query($query)) {
+  if ($resultado = $db->query($query)) {
 
     $tickets = array();
 
@@ -51,10 +34,10 @@ if ($conexao) {
     $query = '';
     $query = "UPDATE av_tickets SET validade = false WHERE (ticket = $ticket);";
 
-    $conexao->query($query);
+    $db->query($query);
 
   }
 
-  $conexao->close();
+  $db->close();
 
 }
