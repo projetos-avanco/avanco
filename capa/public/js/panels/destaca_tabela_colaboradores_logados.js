556f7bfe82deb60e375791242cd96155f14068ac
Wellington Felix <wellington_bhmg@hotmail.com>
2018-03-08 14:57:42 -0300

    removendo cores das linhas da coluna opcões

--- capa/public/js/panels/destaca_tabela_colaboradores_logados.js
+++ capa/public/js/panels/destaca_tabela_colaboradores_logados.js
@@ -3,13 +3,14 @@ $('document').ready(function() {
   $('table tbody tr').each(function() {
 
     var logado = $(this).find('td:nth-child(5)').html();
-    var oculto = $(this).find('td:nth-child(6)').html();
-
+    var status = $(this).find('td:nth-child(6)').html();
+    var classe = $(this).find('td:nth-child(7)').attr('class');
+    
     if (logado == 'Não') {
 
       $(this).find('td').addClass('warning');
 
-    } else if (logado == 'Sim' && oculto == 'Sim') {
+    } else if (logado == 'Sim' && status == 'Offline') {
 
       $(this).find('td').addClass('danger');
 
@@ -17,6 +18,14 @@ $('document').ready(function() {
 
       $(this).find('td').addClass('success');
 
+    }
+
+    if (classe == 'opcao text-center') {
+
+      $(this).find('td.opcao').removeClass('warning');
+      $(this).find('td.opcao').removeClass('danger');
+      $(this).find('td.opcao').removeClass('success');
+      
     }
 
   });
