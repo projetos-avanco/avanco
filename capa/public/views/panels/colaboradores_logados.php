fb90817eb207be4da0040e8d0e0ea3b205782a2b
Wellington Felix <wellington_bhmg@hotmail.com>
2018-03-08 15:01:26 -0300

    exibindo opções (offline/online)

--- capa/public/views/panels/colaboradores_logados.php
+++ capa/public/views/panels/colaboradores_logados.php
@@ -3,8 +3,39 @@
 
 <?php
 
+  # recuperando id do chat do usuário logado no chat
+  $id = $_SESSION['usuario']['id'];
+
+  # verificando o nível de permissão do usuário logado no portal avanção
+  $nivel = $_SESSION['usuario']['nivel'];
+
+  $dados = '';
+
   # chamando funçao que recupera os dados para o painel de colaboradores logados
   $paineis = retornaDadosParaPainelDeColaboradoresLogados();
+
+  # chamando função que retorna os colaboradores integrantes do time do capitão que está logado no portal avanção
+  $dados = retornaIdDosColaboradoresDoTime($id, $nivel, $dados);
+
+  # verificando se o usuário que está logado no portal avanção pode alterar o status de algum colaborador
+  if ($dados['exibir_opcoes']) {
+
+    # repassando colaboradores que podem ter seu status alterado para o array com os colaboradores do suporte
+    for ($i = 0; $i < count($dados['id']); $i++) {
+
+      for ($j = 0; $j < count($paineis['suporte']); $j++) {
+  
+        if ($paineis['suporte'][$j]['id'] == $dados['id'][$i]) {
+  
+          $paineis['suporte'][$j]['exibir'] = true;
+  
+        }
+  
+      }
+  
+    }
+
+  }
 
 ?>
 
@@ -17,7 +48,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Portal Avanção - Colaboradores Logados</title>
+  <title>Portal Avanção - Painel Colaboradores Logados</title>
 
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -35,7 +66,7 @@
   <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/home.css">
   <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/sidebar.css">
   <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/navbar.css">
-  <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/panels/tabela_logados.css">
+  <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/panels/tabela_logados.css">  
 </head>
 
 <body>
@@ -45,8 +76,7 @@
 
         <div class="row">
           <div class="col-sm-12">
-            <h2>Painel de Colaboradores Logados</h2>
-
+            <h2>Painel Colaboradores Logados</h2>
             <hr>
           </div>
         </div><!-- linha -->
@@ -59,57 +89,89 @@
 
             <br>
 
-            <table class="table"><!-- painel do suporte -->
+            <table class="table" id="tabela-suporte"><!-- painel do suporte -->
               <thead>
-                <tr>
-                  <th class="text-center">ID</th>
+                <tr>  
+                  <th class="text-center">Id</th>              
                   <th class="text-center">Colaborador</th>
                   <th class="text-center">Em Atendimento</th>
                   <th class="text-center">Em Espera</th>
                   <th class="text-center">Logado</th>
-                  <th class="text-center">Oculto</th>
+                  <th class="text-center">Status</th>
+
+                <?php if ($dados['exibir_opcoes']) : ?>
+                  <th class="text-center">Opções</th>
+                <?php endif; ?>
                 </tr>
               </thead>
               <tbody>
-              <?php foreach($paineis['suporte'] as $suporte) : ?>
-                <tr>
+              <?php foreach ($paineis['suporte'] as $suporte) : ?>
+                <tr>                  
                   <td class="text-center"><?php echo $suporte['id']; ?></td>
                   <td class="text-center"><?php echo $suporte['nome'] . ' ' . $suporte['sobrenome']; ?></td>
                   <td class="text-center"><?php echo $suporte['atendimento']; ?></td>
                   <td class="text-center"><?php echo $suporte['espera']; ?></td>
                   <td class="text-center"><?php echo $suporte['logado']; ?></td>
-                  <td class="text-center"><?php echo $suporte['oculto']; ?></td>
+                  <td class="text-center"><?php echo $suporte['status']; ?></td>
+
+                <?php if ($dados['exibir_opcoes']) : ?>
+
+                  <?php if ($suporte['exibir']) : ?>
+
+                    <?php if ($suporte['status'] == 'Offline') : ?>
+                    <td class="opcao text-center" width="10%">                  
+                      <a class="btn btn-sm btn-success" href="<?php echo BASE_URL; ?>app/requests/get/recebe_status.php?id=<?php echo $suporte['id']; ?>&status=0">
+                        <i class="fa fa-eye" aria-hidden="true"></i> Online
+                      </a>                    
+                    </td> 
+                    <?php elseif ($suporte['status'] == 'Online') : ?>
+                    <td class="opcao text-center" width="10%">                  
+                      <a class="btn btn-sm btn-danger" href="<?php echo BASE_URL; ?>app/requests/get/recebe_status.php?id=<?php echo $suporte['id']; ?>&status=1">
+                        <i class="fa fa-eye-slash" aria-hidden="true"></i> Offline
+                      </a>                    
+                    </td>                                  
+                    <?php endif; ?><!-- status --> 
+
+                  <?php elseif ($suporte['exibir'] == false) : ?>
+                    <td class="opcao text-center" width="10%"></td>                       
+                  <?php endif; ?><!-- exibir -->
+
+                <?php endif; ?><!-- dados -->
                 </tr>
               <?php endforeach; ?>
               </tbody>
-            </table><!-- painel do suporte -->
+            </table><!-- painel do suporte -->                        
+          </div>
+        </div>
 
-            <br>
+        <br> 
 
+        <div class="row">
+          <div class="col-sm-12">          
             <h3 class="text-center">Externo</h3>
 
             <br>
 
-            <table class="table"><!-- painel do externo -->
+            <table class="table" id="tabela-externo"><!-- painel do externo -->
               <thead>
-                <tr>
-                  <th class="text-center">ID</th>
+                <tr> 
+                  <th class="text-center">Id</th>                 
                   <th class="text-center">Colaborador</th>
                   <th class="text-center">Em Atendimento</th>
                   <th class="text-center">Em Espera</th>
                   <th class="text-center">Logado</th>
-                  <th class="text-center">Oculto</th>
+                  <th class="text-center">Status</th>
                 </tr>
               </thead>
               <tbody>
               <?php foreach($paineis['externo'] as $externo) : ?>
-                <tr>
+                <tr>                  
                   <td class="text-center"><?php echo $externo['id']; ?></td>
                   <td class="text-center"><?php echo $externo['nome'] . ' ' . $externo['sobrenome']; ?></td>
                   <td class="text-center"><?php echo $externo['atendimento']; ?></td>
                   <td class="text-center"><?php echo $externo['espera']; ?></td>
                   <td class="text-center"><?php echo $externo['logado']; ?></td>
-                  <td class="text-center"><?php echo $externo['oculto']; ?></td>
+                  <td class="text-center"><?php echo $externo['status']; ?></td>
                 </tr>
               <?php endforeach; ?>
               </tbody>
@@ -125,9 +187,9 @@
 
   <script src="<?php echo BASE_URL; ?>public/js/sidebar.js"></script>
   <script src="<?php echo BASE_URL; ?>public/js/outros.js"></script>
-  <script src="<?php echo BASE_URL; ?>public/js/panels/destaca_tabela_colaboradores_logados.js"></script>
-  <script src="<?php echo BASE_URL; ?>public/js/panels/atualizacao_automatica.js"></script>
   <script src="<?php echo BASE_URL; ?>public/js/panels/paginacao_tabela.js"></script>
+  <script src="<?php echo BASE_URL; ?>public/js/panels/destaca_tabela_colaboradores_logados.js"></script>  
+  <script src="<?php echo BASE_URL; ?>public/js/panels/atualizacao_automatica.js"></script>
 </body>
 </html>
 
