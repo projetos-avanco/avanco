997dfd95b03c9130f2fb0c825313569d3af2b492
Wellington Felix <wellington_bhmg@hotmail.com>
2018-03-08 15:03:53 -0300

    alterando título da página

--- capa/public/views/panels/ramais.php
+++ capa/public/views/panels/ramais.php
@@ -21,7 +21,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Portal Avanção - Painel de Ramais</title>
+  <title>Portal Avanção - Painel Ramais</title>
 
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -49,7 +49,7 @@
 
         <div class="row">
           <div class="col-sm-12">
-            <h2>Painel de Ramais</h2>
+            <h2>Painel Ramais</h2>
 
             <hr>
           </div>
