aa5b6e7f80c8cd20dce4899f75bc78abd6456ae4
Wellington Felix <wellington_bhmg@hotmail.com>
2018-03-08 14:52:37 -0300

    criando função responsável por retornoar o id dos colaboradores do chat e de alterar o status dos usuários do chat

--- capa/app/modules/panels/modulo_colaboradores_logados.php
+++ capa/app/modules/panels/modulo_colaboradores_logados.php
@@ -41,3 +41,104 @@ function retornaDadosParaPainelDeColaboradoresLogados()
 
   return $paineis;
 }
+
+/**
+ * consulta e retorna os colaboradores integrantes do time do capitão qu está logado no portal avanção
+ * @param - string com o id do chat do usuário logado no portal avanção
+ * @param - string com o nível de permissão de acesso do usuário logado no portal avanção
+ * @param - array com o modelo de dados de opções
+ */
+function retornaIdDosColaboradoresDoTime($id, $nivel, $dados)
+{
+  $dados = criaModeloDeDadosDeOpcoes();
+  $db    = abre_conexao();
+
+  # verificando se um capitão está logado no portal avanção
+  if ($nivel == 1) {
+
+    # verificando qual é o id do time do capitão que está logado
+    switch ($id) {
+
+      case '14':
+  
+        $dados['time']    = 3;
+        $dados['exibir_opcoes'] = true; # permitindo exibição das opções offline/online dos colaboradores do time divergente
+  
+          break;
+  
+      case '17':
+  
+        $dados['time']    = 2;
+        $dados['exibir_opcoes'] = true; # permitindo exibição das opções offline/online dos colaboradores do time os templários
+  
+          break;
+  
+      case '20':
+  
+        $dados['time']    = 5;
+        $dados['exibir_opcoes'] = true; # permitindo exibição das opções offline/online dos colaboradores do time avalanche
+          break;
+  
+      case '25':
+  
+        $dados['time']    = 4;
+        $dados['exibir_opcoes'] = true; # permitindo exibição das opções offline/online dos colaboradores do time gulliver
+          
+          break;
+
+    }
+
+    # verificando se o usuário logado no portal avanção é capitão de algum time
+    if ($dados['time'] != 0) {
+      
+      # chamando função que retorna os ids do chat dos integrantes do time do capitão logado no portal avanção
+      $dados = consultaIdDosColaboradoresDoTime($db, $dados);
+
+    }
+
+  } elseif ($nivel == 2) { # verificando se o usuário logado no portal avanção é um supervisor
+    
+    # chamando função que retorna todos os ids do chat dos colaboradores do suporte
+    $dados = consultaIdDeTodosOsColaboradores($db, $dados);
+
+    # permitindo exibição das opções offline/online de todos os colaboradores
+    $dados['exibir_opcoes'] = true;
+
+  }
+
+  return $dados;
+
+}
+
+/**
+ * responsável por alterar o status de um usuário do chat para offline ou online
+ * @param - string com o id do chat
+ * @param - string com o status (0 - online / 1 - offline)
+ */
+function alteraStatus($id, $status)
+{
+  require DIRETORIO_FUNCTIONS . 'panels/atualiza_colaboradores_logados.php';
+
+  $db = abre_conexao();
+
+  # verificando se foi solicitado a alteração para offline/online
+  if ($status) {
+
+    # chamando função que altera o status do usuário do chat para offline
+    alteraStatusParaOffline($db, $id);
+
+  } else {
+
+    # chamando função que alterar o status do usuário do chat para online
+    alteraStatusParaOnline($db, $id);
+
+  }
+
+  fecha_conexao($db);
+
+  # redirecionando usuário para o painel de logados
+  header('Location: ' . BASE_URL . 'public/views/panels/colaboradores_logados.php');
+
+  exit;
+
+}
