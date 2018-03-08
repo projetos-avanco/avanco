6c37cd06da266de1e0eafae2086d4fa7046709df
Wellington Felix <wellington_bhmg@hotmail.com>
2018-03-08 14:51:09 -0300

    criando função que retorna o modelo de opções (offline/online)

--- capa/app/models/panels/modelo_colaboradores_logados.php
+++ capa/app/models/panels/modelo_colaboradores_logados.php
@@ -8,4 +8,22 @@ function criaModeloDeColaboradoresLogados()
   $painel = array();
 
   return $painel;
+  
+}
+
+/*
+ * cria um array modelo para receber os dados para opção ocultar
+ */
+function criaModeloDeDadosDeOpcoes()
+{
+  $dados = array(
+
+    'id' => array(),
+    'time' => 0,
+    'exibir_opcoes' => false,
+
+  );
+
+  return $dados;
+
 }
