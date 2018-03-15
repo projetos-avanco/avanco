0ea2263b44317ff2ab9b3c81eba2b0f1d362b1d9
Wellington Felix <wellington_bhmg@hotmail.com>
2018-03-15 09:59:51 -0300

    alterado função para permitir o acesso de usuários nível 1 que sejam capitães nas páginas de metas dos capitães.

--- capa/app/helpers/seguranca.php
+++ capa/app/helpers/seguranca.php
@@ -12,7 +12,7 @@ function criaCodigoHash($senha)
 /**
  * verifica se o usuário está logado
  */
-function verificaUsuarioLogado($pagina)
+function verificaUsuarioLogado($pagina, $id = null)
 {
   require DIRETORIO_MODELS . 'sessao.php';
 
@@ -56,9 +56,18 @@ function verificaUsuarioLogado($pagina)
 
       }
 
+      # páginas que os usuários nível 1 que são capitães possuem permissão para acessar
+      if (($pagina == 'metas_capitaes_selecao.php') AND ($id == 14 OR $id == 17 OR $id == 20 OR $id == 25) OR
+          ($pagina == 'metas_capitaes.php') AND ($id == 14 OR $id == 17 OR $id == 20 OR $id == 25)) {
+
+        return true;
+
+      }
+
       # páginas que usuários de nível 1 não possuem permissão para acessar
       if ($pagina == 'nova_atividade.php' OR
-          $pagina == 'registro_horas.php') {
+          $pagina == 'registro_horas.php' OR
+          $pagina == 'metas_capitaes_selecao.php') {
 
         $_SESSION['mensagens']['mensagem'] = '<p class="text-center"><strong>Sinto Muito!</strong> Seu nível de usuário não permite acessar esse módulo.</p>';
         $_SESSION['mensagens']['tipo']     = 'danger';
@@ -80,7 +89,9 @@ function verificaUsuarioLogado($pagina)
          $pagina == 'extrato.php'               OR
          $pagina == 'conta.php'                 OR
          $pagina == 'loja.php'                  OR
-         $pagina == 'registro_horas.php') {
+         $pagina == 'registro_horas.php'        OR
+         $pagina == 'metas_capitaes.php'        OR
+         $pagina == 'metas_capitaes_selecao.php') {
 
         return true;
 
