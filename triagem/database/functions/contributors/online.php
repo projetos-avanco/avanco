83266505f0abec86549269a211e95f1e23116a63
Wellington Felix <wellington_bhmg@hotmail.com>
2018-03-20 10:36:14 -0300

    melhorando consulta de colaboradores agendados

--- triagem/database/functions/contributors/online.php
+++ triagem/database/functions/contributors/online.php
@@ -8,24 +8,35 @@
 function verificaColaboradorAgendadoOnlineNoChat($array, $db)
 {
   $query =
-    "SELECT DISTINCT
-      u.user_id AS id,
-      s.name AS nome,
-      s.surname AS sobrenome,
-      d.id AS id_departamento,
-      d.name AS departamento
+    "SELECT 
+      DISTINCT
+        u.user_id AS id,
+        s.name AS nome,
+        s.surname AS sobrenome,
+        d.id AS id_departamento,
+        d.name AS departamento		
     FROM lh_departament AS d
     INNER JOIN lh_userdep AS u
       ON u.dep_id = d.id
     INNER JOIN lh_users AS s
       ON s.id = u.user_id
-    WHERE NOT (d.id = 1 OR d.id = 2 OR d.id = 3 OR d.id = 4 OR d.id = 6 OR d.id = 7 OR d.id = 8 OR d.id = 9 OR d.id = 10 OR d.id = 11)
+    WHERE NOT (d.id = 1  OR 
+               d.id = 2  OR
+               d.id = 3  OR 
+               d.id = 4  OR 
+               d.id = 6  OR 
+               d.id = 7  OR 
+               d.id = 8  OR 
+               d.id = 9  OR 
+               d.id = 10 OR 
+               d.id = 11)
       AND (s.id = {$array[0]['id']})
       AND (d.disabled = 0)
       AND (s.disabled = 0)
       AND (u.last_activity > 0)
       AND (FROM_UNIXTIME(u.last_activity, '%Y-%m-%d') = CURRENT_DATE())
-    ORDER BY id";
+      AND (TIMEDIFF(FROM_UNIXTIME(u.last_activity, '%H:%i:%s'), CURRENT_TIME()) < '00:00:30')
+    ORDER BY id;";
 
   if ($resultado = $db->query($query)) {
 
