8ac131d250ebaff70ce96d6edb878555eaadd276
Wellington Felix <wellington_bhmg@hotmail.com>
2018-04-17 10:57:21 -0300

    alterando o modelo de relatório de ranking dos colaboradores

--- capa/app/models/reports/ranking/modelo_ranking.php
+++ capa/app/models/reports/ranking/modelo_ranking.php
@@ -5,36 +5,7 @@
  */
 function criaModeloDoRelatorioDeRanking()
 {
-  $ranking = array(
-
-    'pessoal' => array(
-      'id' => '',
-      'nome' => '',
-      'sobrenome' => '',
-      'moedas' => ''
-    ),
-
-    'atendimento' => array(
-      'atendimentos_demandados' => '',
-      'atendimentos_realizados' => '',
-      'atendimentos_perdidos' => '',
-      'percentual_perda' => '',
-      'percentual_fila_ate_15_minutos' => '',
-      'tma' => ''
-    ),
-
-    'indices' => array(
-      'percentual_avancino' => '',
-      'percentual_eficiencia' => '',
-      'percentual_questionario_respondido' => ''
-    ),
-
-    'periodo' => array(
-      'data_1' => '',
-      'data_2' => ''
-    )
-
-  );
+  $ranking = array();
 
   return $ranking;
 
