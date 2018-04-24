40216963a8391af663a989d72781f4830fc245e4
Wellington Felix <wellington_bhmg@hotmail.com>
2018-04-24 15:56:41 -0300

    adicionando script que processa os dados do registro de horas

---
+++ capa/app/requests/get/processa_lancamentos_horas.php
@@ -0,0 +1,21 @@
+<?php
+
+require DIRETORIO_MODULES . 'hours/horas.php';
+
+# verificando se houve uma requisição via método GET
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+  # recuperando o número da issue
+  $issue = isset($_GET['issue']) ? $_GET['issue'] : null;
+
+  # verificando se o número da issue foi recuperado
+  if (! empty($issue) && $issue != null) {
+
+    $dados = array();
+
+    # chamando função que recupera os dados de uma única issue
+    $dados = recuperaDadosDoRegistroDeHoras($issue, $dados);
+    
+  }
+
+}
