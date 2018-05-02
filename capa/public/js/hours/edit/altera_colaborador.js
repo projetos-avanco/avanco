66f7bc15d3b268bc74a21c0aa6a220a6d34fd06f
Wellington Felix <wellington_bhmg@hotmail.com>
2018-05-02 16:39:52 -0300

    script que altera o colaborador da issue do registro de horas

---
+++ capa/public/js/hours/edit/altera_colaborador.js
@@ -0,0 +1,19 @@
+$('document').ready(function() {
+
+  // aguardando alterações no select dos colaboradores
+  $('#colaborador').change(function() {
+
+    var id       = '';
+    var conteudo = '';
+
+    // recuperando id do colaborador e conteúdo do option
+    id       = $('#colaborador').val();
+    conteudo = $('#colaborador :selected').text();
+
+    // alterando os valores do select do colaborador registrado
+    $('#colaborador-registrado').empty();
+    $('#colaborador-registrado').append('<option value="'+id+'" selected>'+conteudo+'</option>');
+
+  });
+
+});
