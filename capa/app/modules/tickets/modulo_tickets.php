782dbdcf77f699d0e3a9f4a956baa74cb50febed
Wellington Felix <wellington_bhmg@hotmail.com>
2018-05-11 15:06:41 -0300

    alterando funções para retornarem os dados para as páginas de consulta e visualização de tickets

--- capa/app/modules/tickets/modulo_tickets.php
+++ capa/app/modules/tickets/modulo_tickets.php
@@ -1,12 +1,12 @@
 <?php
 
 /*
- * reponsável por consultar e recuperar todos os tickets gerados pelo sistema
+ * responsável por consultar e recuperar os dados da página de consulta 
  */
-function recuperaTodosOsTicketsGerados()
+function recuperaDadosDaPaginaDeConsultaDeTickets()
 {
   require DIRETORIO_MODELS    . 'tickets/modelo_tickets.php';
-  require DIRETORIO_FUNCTIONS . 'tickets/instrucoes_tickets.php';
+  require DIRETORIO_FUNCTIONS . 'tickets/consultas_tickets.php';
 
   # chamando função que cria o modelo de dados de tickets
   $tickets = criaModeloDeTickets();
@@ -14,12 +14,32 @@ function recuperaTodosOsTicketsGerados()
   # abrindo conexão com a base de dados
   $db = abre_conexao();
 
-  # chamando função que consulta todos os tickets gerados
-  $tickets = consultaTodosOsTicketsGerados($db, $tickets);
+  $tickets = consultaDadosBasicosDosTickets($db, $tickets);
 
   fecha_conexao($db);
 
   return $tickets;
+}
+
+/**
+ * reponsável por consultar e recuperar todos os dados de um ticket
+ * @param - array modelo que irá receber os dados do ticket
+ * @param - string com o número do ticket
+ */
+function recuperaDadosDaPaginaDeVisualizacaoDeTickets($dados, $ticket)
+{
+  require DIRETORIO_MODELS    . 'tickets/modelo_tickets.php';
+  require DIRETORIO_FUNCTIONS . 'tickets/consultas_tickets.php';
+  
+  # abrindo conexão com a base de dados
+  $db = abre_conexao();
+
+  # chamando função que consulta todos os tickets gerados
+  $dados = consultaDadosDaPaginaDeVisualizaoDeTickets($db, $dados, $ticket);
+
+  fecha_conexao($db);
+
+  return $dados;
 
 }
 
