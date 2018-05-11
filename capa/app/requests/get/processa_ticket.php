a0d75a03ca59298e9c1fc087ffcaf9d0adb4e06b
Wellington Felix <wellington_bhmg@hotmail.com>
2018-05-11 15:07:42 -0300

    criando retorno de dados para funções de visualização e deleção

--- capa/app/requests/get/processa_ticket.php
+++ capa/app/requests/get/processa_ticket.php
@@ -3,9 +3,6 @@
 # verificando se houve requisição via método GET
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
-  require '../../../init.php';
-  require DIRETORIO_MODULES . 'tickets/modulo_tickets.php';
-
   # recuperando o número do ticket  e função que será executada
   $ticket = isset($_GET['ticket']) ? $_GET['ticket'] : '';
   $funcao = isset($_GET['funcao']) ? $_GET['funcao'] : '';
@@ -13,14 +10,38 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   # verificando se o número do ticket e o nome da função foram enviados
   if (! empty($ticket) AND ! empty($funcao)) {
 
+    # verificando se será executada a função visualiza
+    if ($funcao == 'visualiza') {
+
+      require DIRETORIO_MODULES . 'tickets/modulo_tickets.php';
+
+      $dados = array();
+
+      $dados = recuperaDadosDaPaginaDeVisualizacaoDeTickets($dados, $ticket);
+      
+    # verificando se será executada a função edita
+    } elseif ($funcao == 'edita') {
+      
+      require DIRETORIO_MODULES . 'tickets/modulo_tickets.php';
+
+      $dados = array();
+
+      $dados = recuperaDadosDaPaginaDeVisualizacaoDeTickets($dados, $ticket);
+      
     # verificando se será executada a função deleta
-    if ($funcao == 'deleta') {
+    } elseif ($funcao == 'deleta') {
+
+      require '../../../init.php';
+      require DIRETORIO_MODULES . 'tickets/modulo_tickets.php';
 
       # chamando função que deleta um ticket
       deletaTicket($ticket);
 
     # verificando se será executada a função invalida
     } elseif ($funcao == 'invalida') {
+
+      require '../../../init.php';
+      require DIRETORIO_MODULES . 'tickets/modulo_tickets.php';
 
       # chamando função que invalida um ticket
       invalidaTicket($ticket);
