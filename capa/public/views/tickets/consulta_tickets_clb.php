4ed5e08d1ce51538279800ade00896624973b60b
Wellington Felix <wellington_bhmg@hotmail.com>
2018-05-23 09:30:09 -0300

    alterando o nome do título

--- capa/public/views/tickets/consulta_tickets_clb.php
+++ capa/public/views/tickets/consulta_tickets_clb.php
@@ -44,7 +44,7 @@
 
         <div class="row">
           <div class="col-sm-12">
-            <h2>Consulta de Tickets</h2>
+            <h2>Consulta Tickets</h2>
 
             <hr>
           </div>
