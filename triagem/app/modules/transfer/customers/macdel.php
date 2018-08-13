3dcadcb9dfbc6a16b9dedd7253f5ac035a0238d6
Wellington Felix <wellington_bhmg@hotmail.com>
2018-08-13 15:02:55 -0300

    adicionando script que redireciona o cliente macdel na curva abc

---
+++ triagem/app/modules/transfer/customers/macdel.php
@@ -0,0 +1,120 @@
+<?php
+
+$contador = count($colaboradores);
+
+if ($cliente['produto'] == '1') {
+  switch ($cliente['modulo']) {
+    # Materiais, TNFE, WMS
+    case '1':
+    case '6':
+    case '7':
+      for ($i = 0; $i < $contador; $i++) {
+        if ($colaboradores[$i]['id'] != '23' && $colaboradores[$i]['id'] != '20') {
+          unset($colaboradores[$i]);
+        }
+      }
+
+      $quantidade = count($colaboradores);
+
+      $colaboradores = array_values($colaboradores);
+      $colaboradores = verificaFilaDosColaboradores($colaboradores, $quantidade, $conexao);
+
+      if (count($colaboradores) == 0) {
+        echo json_encode(NULL);
+        exit;
+      } elseif (count($colaboradores) == 1) {
+        redirecionaClienteParaDepartamento($colaboradores, $cliente, true);
+      } else {
+        usort($colaboradores, "comparaChavesDosArraysInternos");
+        redirecionaClienteParaDepartamento($colaboradores, $cliente, true);
+      }
+        break;
+
+    # Fiscal, Contábil
+    case '2':
+    case '4':
+      for ($i = 0; $i < $contador; $i++) {
+        if ($colaboradores[$i]['id'] != '25') {
+          unset($colaboradores[$i]);
+        }
+      }
+
+      $quantidade = count($colaboradores);
+      $colaboradores = array_values($colaboradores);
+
+      if (count($colaboradores) == 0) {
+        echo json_encode(NULL);
+        exit;
+      } elseif (count($colaboradores) == 1) {
+        redirecionaClienteParaDepartamento($colaboradores, $cliente, true);
+      } else {
+        usort($colaboradores, "comparaChavesDosArraysInternos");
+        redirecionaClienteParaDepartamento($colaboradores, $cliente, true);
+      }
+        break;
+
+    # Financeiro
+    case '3':
+      for ($i = 0; $i < $contador; $i++) {
+        if ($colaboradores[$i]['id'] != '14') {
+          unset($colaboradores[$i]);
+        }
+      }
+
+      $quantidade = count($colaboradores);
+      $colaboradores = array_values($colaboradores);
+
+      if (count($colaboradores) == 0) {
+        echo json_encode(NULL);
+        exit;
+      } elseif (count($colaboradores) == 1) {
+        redirecionaClienteParaDepartamento($colaboradores, $cliente, true);
+      } else {
+        usort($colaboradores, "comparaChavesDosArraysInternos");
+        redirecionaClienteParaDepartamento($colaboradores, $cliente, true);
+      }
+        break;
+
+    # Cotação
+    case '5':
+      for ($i = 0; $i < $contador; $i++) {
+        if ($colaboradores[$i]['id'] != '23') {
+          unset($colaboradores[$i]);
+        }
+      }
+
+      $quantidade = count($colaboradores);
+      $colaboradores = array_values($colaboradores);
+
+      if (count($colaboradores) == 0) {
+        echo json_encode(NULL);
+        exit;
+      } elseif (count($colaboradores) == 1) {
+        redirecionaClienteParaDepartamento($colaboradores, $cliente, true);
+      } else {
+        usort($colaboradores, "comparaChavesDosArraysInternos");
+        redirecionaClienteParaDepartamento($colaboradores, $cliente, true);
+      }
+        break;
+  }
+} elseif ($cliente['produto'] == '2') {
+  # Qualquer Módulo
+  for ($i = 0; $i < $contador; $i++) {
+    if ($colaboradores[$i]['id'] != '20') {
+      unset($colaboradores[$i]);
+    }
+  }
+
+  $quantidade = count($colaboradores);
+  $colaboradores = array_values($colaboradores);
+
+  if (count($colaboradores) == 0) {
+    echo json_encode(NULL);
+    exit;
+  } elseif (count($colaboradores) == 1) {
+    redirecionaClienteParaDepartamento($colaboradores, $cliente, true);
+  } else {
+    usort($colaboradores, "comparaChavesDosArraysInternos");
+    redirecionaClienteParaDepartamento($colaboradores, $cliente, true);
+  }
+}
