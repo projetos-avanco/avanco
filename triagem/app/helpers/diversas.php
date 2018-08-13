324162f79a02bf12ad0145f0e2c777ca73c5fe7b
Wellington Felix <wellington_bhmg@hotmail.com>
2018-08-13 15:01:09 -0300

    alterando código para informar se o cliente redirecionado terá o parâmetro curva abc = 0 ou 1

--- triagem/app/helpers/diversas.php
+++ triagem/app/helpers/diversas.php
@@ -47,7 +47,7 @@ function redirecionaClienteParaDepartamentoTecnologia($cliente)
 
   # montando URL
   $url =
-    "index.php/por/chat/startchat/(leaveamessage)/true?prefill%5Busername%5D={$cliente['razao_social']}&value_items_admin[0]={$cliente['duvida']}&value_items_admin[1]={$cliente['nome_usuario']}&value_items_admin[2]={$cliente['conta_contrato']}&value_items_admin[3]={$cliente['razao_social']}&value_items_admin[4]={$cliente['cnpj']}&value_items_admin[5]=0&nome_departamento=Tecnologia&codigo_ticket=0&novo_erp={$cliente['novo_erp']}&prefill%5Bphone%5D=2&value_items_admin[6]={$cliente['telefone']}";
+    "index.php/por/chat/startchat/(leaveamessage)/true?prefill%5Busername%5D={$cliente['razao_social']}&value_items_admin[0]={$cliente['duvida']}&value_items_admin[1]={$cliente['nome_usuario']}&value_items_admin[2]={$cliente['conta_contrato']}&value_items_admin[3]={$cliente['razao_social']}&value_items_admin[4]={$cliente['cnpj']}&value_items_admin[5]=0&nome_departamento=Tecnologia&codigo_ticket=0&novo_erp={$cliente['novo_erp']}&prefill%5Bphone%5D=2&value_items_admin[6]={$cliente['telefone']}&value_items_admin[7]=0";
 
   # redirecionando cliente para o colaborador no chat teste
   echo json_encode(['url' => 'http://192.168.0.47:9999/' . $url], JSON_UNESCAPED_UNICODE);
@@ -64,20 +64,30 @@ function redirecionaClienteParaDepartamentoTecnologia($cliente)
  * @param - array com os dados dos colaboradores que estão online
  * @param - array com os dados do cliente
  */
-function redirecionaClienteParaDepartamento($colaboradores, $cliente)
+function redirecionaClienteParaDepartamento($colaboradores, $cliente, $curva = null)
 {
 
   if (! isset($cliente['ticket'])) {
 
-    # montando URL para atendimentos sem agendamento
-    $url =
-      "index.php/por/chat/startchat/(leaveamessage)/true?prefill%5Busername%5D={$cliente['razao_social']}&value_items_admin[0]={$cliente['duvida']}&value_items_admin[1]={$cliente['nome_usuario']}&value_items_admin[2]={$cliente['conta_contrato']}&value_items_admin[3]={$cliente['razao_social']}&value_items_admin[4]={$cliente['cnpj']}&value_items_admin[5]={$cliente['produto']}&nome_departamento={$colaboradores[0]['departamento']}&codigo_ticket=0&novo_erp={$cliente['novo_erp']}&prefill%5Bphone%5D={$colaboradores[0]['id_departamento']}&value_items_admin[6]={$cliente['telefone']}";
+    if ($curva) {
+
+      # montando URL para atendimentos sem agendamento da curva abc
+      $url =
+        "index.php/por/chat/startchat/(leaveamessage)/true?prefill%5Busername%5D={$cliente['razao_social']}&value_items_admin[0]={$cliente['duvida']}&value_items_admin[1]={$cliente['nome_usuario']}&value_items_admin[2]={$cliente['conta_contrato']}&value_items_admin[3]={$cliente['razao_social']}&value_items_admin[4]={$cliente['cnpj']}&value_items_admin[5]={$cliente['produto']}&nome_departamento={$colaboradores[0]['departamento']}&codigo_ticket=0&novo_erp={$cliente['novo_erp']}&prefill%5Bphone%5D={$colaboradores[0]['id_departamento']}&value_items_admin[6]={$cliente['telefone']}&value_items_admin[7]=1";
+
+    } else {
+
+      # montando URL para atendimentos sem agendamento
+      $url =
+        "index.php/por/chat/startchat/(leaveamessage)/true?prefill%5Busername%5D={$cliente['razao_social']}&value_items_admin[0]={$cliente['duvida']}&value_items_admin[1]={$cliente['nome_usuario']}&value_items_admin[2]={$cliente['conta_contrato']}&value_items_admin[3]={$cliente['razao_social']}&value_items_admin[4]={$cliente['cnpj']}&value_items_admin[5]={$cliente['produto']}&nome_departamento={$colaboradores[0]['departamento']}&codigo_ticket=0&novo_erp={$cliente['novo_erp']}&prefill%5Bphone%5D={$colaboradores[0]['id_departamento']}&value_items_admin[6]={$cliente['telefone']}&value_items_admin[7]=0";
+
+    }
 
   } else {
 
     # montando URL para atendimentos com agendamento
     $url =
-      "index.php/por/chat/startchat/(leaveamessage)/true?prefill%5Busername%5D={$cliente['razao_social']}&value_items_admin[0]={$cliente['duvida']}&value_items_admin[1]={$cliente['nome']}&value_items_admin[2]={$cliente['conta_contrato']}&value_items_admin[3]={$cliente['razao_social']}&value_items_admin[4]={$cliente['cnpj']}&value_items_admin[5]={$cliente['produto']}&nome_departamento={$colaboradores[0]['departamento']}&codigo_ticket={$cliente['ticket']}&novo_erp={$cliente['novo_erp']}&prefill%5Bphone%5D={$colaboradores[0]['id_departamento']}&value_items_admin[6]={$cliente['telefone']}";
+      "index.php/por/chat/startchat/(leaveamessage)/true?prefill%5Busername%5D={$cliente['razao_social']}&value_items_admin[0]={$cliente['duvida']}&value_items_admin[1]={$cliente['nome']}&value_items_admin[2]={$cliente['conta_contrato']}&value_items_admin[3]={$cliente['razao_social']}&value_items_admin[4]={$cliente['cnpj']}&value_items_admin[5]={$cliente['produto']}&nome_departamento={$colaboradores[0]['departamento']}&codigo_ticket={$cliente['ticket']}&novo_erp={$cliente['novo_erp']}&prefill%5Bphone%5D={$colaboradores[0]['id_departamento']}&value_items_admin[6]={$cliente['telefone']}&value_items_admin[7]=0";
 
   }
 
