8422473e86835e542ce4cb7ef8ba47d1de5f7af5
Wellington Felix <wellington_bhmg@hotmail.com>
2018-08-28 09:10:18 -0300

    adicionando as opções da agenda no menu lateral

--- capa/inc/templates/sidebar.php
+++ capa/inc/templates/sidebar.php
@@ -91,16 +91,52 @@
       <li class="bordermenu">
         <a href="#" class="accordion-heading" data-toggle="collapse" data-target="#submenu4">
           <span class="nav-header-primary">
-            <i class="fa fa-tags" aria-hidden="true"></i> Tickets
+            <i class="fa fa-book" aria-hidden="true"></i> Agenda
             <span class="pull-right">
                 <i id="setinha" class="fa fa-caret-down" aria-hidden="true"></i>
             </span>
           </span>
         </a>
         <ul class="nav collapse"  id="submenu4">
+        <?php if (isset($_SESSION['usuario']['nivel']) AND $_SESSION['usuario']['nivel'] == 2) : ?>
+          <li>
+            <a href="<?php echo BASE_URL; ?>../capa/public/views/schedule/tela_inicial.php">
+              <p>Atividades<p>
+            </a>
+          </li>
+        <?php elseif(isset($_SESSION['usuario']['nivel']) AND $_SESSION['usuario']['nivel'] == 2) : ?>
+          <li>
+            <a href="<?php echo BASE_URL; ?>../capa/public/views/avancoins/nova_atividade.php">
+              <p>Opção 2<p>
+            </a>
+          </li>
+          <li>
+            <a href="<?php echo BASE_URL; ?>../capa/public/views/avancoins/extrato.php">
+              <p>Opção 3<p>
+            </a>
+          </li>
+        <?php endif; ?>
+        <li>
+          <a href="<?php echo BASE_URL; ?>../dashboard/public/views/store/loja.php" target="_blank">
+            <p>Opção 4<p>
+          </a>
+        </li>
+        </ul>
+      </li>
+
+      <li class="bordermenu">
+        <a href="#" class="accordion-heading" data-toggle="collapse" data-target="#submenu5">
+          <span class="nav-header-primary">
+            <i class="fa fa-tags" aria-hidden="true"></i> Tickets
+            <span class="pull-right">
+                <i id="setinha" class="fa fa-caret-down" aria-hidden="true"></i>
+            </span>
+          </span>
+        </a>
+        <ul class="nav collapse"  id="submenu5">
           <li>
             <a href="<?php echo BASE_URL; ?>../tickets/public/views/screen/novo_ticket.php">
-              <p>Novo<p>              
+              <p>Novo<p>
             </a>
           </li>
           <li>
@@ -118,7 +154,7 @@
       </li>
 
       <li class="bordermenu">
-        <a href="#" class="accordion-heading" data-toggle="collapse" data-target="#submenu5">
+        <a href="#" class="accordion-heading" data-toggle="collapse" data-target="#submenu6">
           <span class="nav-header-primary">
             <i class="fa fa-clock-o" aria-hidden="true"></i> Horas
             <span class="pull-right">
@@ -126,7 +162,7 @@
             </span>
           </span>
         </a>
-        <ul class="nav collapse"  id="submenu5">
+        <ul class="nav collapse"  id="submenu6">
           <li>
             <a href="<?php echo BASE_URL; ?>../capa/public/views/hours/registro_horas.php">
               <p>Novo<p>
@@ -141,7 +177,7 @@
       </li>
 
       <li class="bordermenu">
-        <a href="#" class="accordion-heading" data-toggle="collapse" data-target="#submenu6">
+        <a href="#" class="accordion-heading" data-toggle="collapse" data-target="#submenu7">
           <span class="nav-header-primary">
             <i class="fa fa-television" aria-hidden="true"></i> Painéis
             <span class="pull-right">
@@ -149,12 +185,12 @@
             </span>
           </span>
         </a>
-        <ul class="nav collapse"  id="submenu6">        
+        <ul class="nav collapse"  id="submenu7">
           <li>
             <a href="<?php echo BASE_URL; ?>../capa/public/views/panels/metas_capitaes_selecao.php">
               <p>Metas<p>
             </a>
-          </li>        
+          </li>
           <li>
             <a href="<?php echo BASE_URL; ?>../capa/public/views/panels/colaboradores_logados.php">
               <p>Logados<p>
@@ -169,7 +205,7 @@
       </li>
 
       <li class="bordermenu">
-        <a href="#" class="accordion-heading" data-toggle="collapse" data-target="#submenu7">
+        <a href="#" class="accordion-heading" data-toggle="collapse" data-target="#submenu8">
           <span class="nav-header-primary">
             <i class="fa fa-bar-chart" aria-hidden="true"></i> Relatórios
             <span class="pull-right">
@@ -178,7 +214,7 @@
           </span>
         </a>
 
-        <ul class="nav collapse"  id="submenu7">        
+        <ul class="nav collapse"  id="submenu8">
           <li>
             <a href="<?php echo BASE_URL; ?>../capa/public/views/reports/calls/consulta_atendimentos.php">
               <p>Atendimentos<p>
@@ -191,7 +227,7 @@
             </a>
           </li>
         <?php endif; ?>
-        </ul>        
+        </ul>
       </li>
 
 
