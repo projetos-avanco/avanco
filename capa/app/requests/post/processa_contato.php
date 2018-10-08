32220d09c1a82eb2f8afa7a87e33751ffab2e41e
Wellington Felix <wellington_bhmg@hotmail.com>
2018-10-08 11:18:02 -0300

    adicionando comentários na função.

--- capa/app/requests/post/processa_contato.php
+++ capa/app/requests/post/processa_contato.php
@@ -1,10 +1,12 @@
 <?php
 
+# verificando se houve requisição via método get
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   require '../../../init.php';
-  require DIRETORIO_MODULES . 'schedule/modulo_contatos.php';
+  require DIRETORIO_MODULES . 'schedule/modulo_contato.php';
 
   if (! empty($_GET['id-cnpj'])) {
+    # chamando função responsável por retornar todos os contatos de um cnpj
     retornaContatos($_GET['id-cnpj']);
   }
 }
