00495e93d894144549926f2a63568256595405ef
Wellington Felix <wellington_bhmg@hotmail.com>
2018-10-08 11:28:37 -0300

    transferindo recuperação de id do cnpj para outro script js.

--- capa/public/js/schedule/address/mascaras.js
+++ capa/public/js/schedule/address/mascaras.js
@@ -1,8 +1,3 @@
 $('document').ready(function() {
   $('#cep').mask('00000-000');
-
-  var url = window.location.href;
-  var arr = url.split('=');
-
-  document.getElementById('id-cnpj').value = arr[1];
-})
+});
