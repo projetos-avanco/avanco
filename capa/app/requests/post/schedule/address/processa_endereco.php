ade8920f0c258a92df8565c652c9ac5bef1f8c42
Wellington Felix <wellington_bhmg@hotmail.com>
2018-10-09 16:20:58 -0300

    refatorando código.

--- capa/app/requests/post/schedule/address/processa_endereco.php
+++ capa/app/requests/post/schedule/address/processa_endereco.php
@@ -2,10 +2,15 @@
 
 # verificando se foi enviado requisição via método GET
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  require '../../../init.php';
-  require DIRETORIO_MODULES . '/schedule/modulo_endereco.php';
+  # importando script de configurações
+  require '../../../../../init.php';
+  
+  # verificando se o id do cnpj da empresa foi enviado
+  if ((!empty($_GET['id-cnpj']))) {
+    # importando script
+    require DIRETORIO_MODULES . '/schedule/modulo_endereco.php';
 
-  if ((! empty($_GET['id-cnpj']))) {
+    # chamando função que retorna o endereço de um cnpj
     consultaEnderecoAjax($_GET['id-cnpj']);
   }
 }
