fc7b6abceb8d2246446b4802f4446f3b70911c3d
Wellington Felix <wellington_bhmg@hotmail.com>
2018-10-17 15:03:28 -0300

    adicionando função responsável pela edição de um contato.

--- capa/app/modules/schedule/modulo_contato.php
+++ capa/app/modules/schedule/modulo_contato.php
@@ -1,6 +1,63 @@
 <?php
 
 /**
+ * responsável por atualizar os dados de um contato nas tabelas de telefones fixos, telefones móveis e e-mails
+ * @param - string com o id do contato
+ * @param - array com o(s) telefone(s) fixo(s)
+ * @param - array com o(s) telefone(s) movel(éis)
+ * @param - array com o(s) endereço(s) de email(s) 
+ */
+function recebeContatoParaEdicao($id, $nome, $fixos, $moveis, $emails)
+{
+  require_once DIRETORIO_FUNCTIONS . 'schedule/contact/insercoes_contato.php';
+  require_once DIRETORIO_FUNCTIONS . 'schedule/contact/delecoes_contato.php';
+
+  $db = abre_conexao();
+
+  $resultados = array();
+
+  # chamando funções que deletam os números fixos, móveis e e-mails do contato
+  $resultados['fixos']  = deletaFixos($db, $id);
+  $resultados['moveis'] = deletaMoveis($db, $id);
+  $resultados['emails'] = deletaEmails($db, $id);
+
+  $_SESSION['atividades']['exibe'] = true;
+
+  # verificando se números fixos, móveis e e-mails do contato foram deletados
+  if ($resultados['fixos'] && $resultados['moveis'] && $resultados['emails']) {
+    # chamando funções para inserir os novos números fixos, móveis e e-mails do contato
+    insereTelefonesFixos($db, $id, $fixos);
+    insereTelefonesMoveis($db, $id, $moveis);
+    insereEmails($db, $id, $emails);
+
+    $_SESSION['atividades']['tipo'] = 'success';
+    $_SESSION['atividades']['mensagens'][] = 'Os dados do contato foram editados com sucesso. Feche essa aba e na página de Atendimento selecione a Empresa novamente';
+
+    # redirecionando usuário para página de edição de contato
+    header('location:' . BASE_URL . 'public/views/schedule/contact/edita_contato.php?id=' . $id); exit;
+  } else {
+    $_SESSION['atividades']['tipo'] = 'danger';
+    $_SESSION['atividades']['mensagens'][] = 'Os dados do contato não foram editados.';
+  }
+}
+
+/**
+ * responsável por solicitar a busca dos dados de um contato e retornar via json
+ * @param - string com o id do contato
+ */
+function recebeContatoParaBusca($id)
+{
+  require_once DIRETORIO_FUNCTIONS . 'schedule/contact/consultas_contato.php';
+
+  $db = abre_conexao();
+
+  # chamando função que retorna todos os dados de um contato
+  $contato = consultaDadosDeUmContato($db, $id);
+
+  echo json_encode($contato); exit;
+}
+
+/**
  * responsável por deletar um contato das tabelas de contatos, emails, telefones fixos e movéis
  * @param - string com o id do contato
  */
@@ -75,7 +132,7 @@ function recebeContato($id, $nome, $fixos, $moveis, $emails)
     }
   } else {
     $_SESSION['atividades']['tipo'] = 'success';
-    $_SESSION['atividades']['mensagens'][] = 'Contato cadastrado com sucesso. Feche essa aba e na página de Atendimento selectione a Empresa novamente.';
+    $_SESSION['atividades']['mensagens'][] = 'Contato cadastrado com sucesso. Feche essa aba e na página de Atendimento selecione a Empresa novamente.';
   }
 
   # redirecionando usuário para página de contato
