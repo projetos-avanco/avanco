051c423fd8bf6db005b1ddaf40926282283463c6
Wellington Felix <wellington_bhmg@hotmail.com>
2018-10-19 10:16:06 -0300

    adicionado função que recebe um endereço para edição.

--- capa/app/modules/schedule/modulo_endereco.php
+++ capa/app/modules/schedule/modulo_endereco.php
@@ -1,8 +1,54 @@
 <?php
 
 /**
+ * responsável por atualizar o endereço nas tabelas de estados, cidades, bairros e endereços
+ * @param - array com os dados do endereço
+ */
+function recebeNovoEnderecoParaEdicao($endereco)
+{
+  require DIRETORIO_FUNCTIONS . 'schedule/address/consultas_endereco.php';
+  require DIRETORIO_FUNCTIONS . 'schedule/address/atualizacoes_endereco.php';
+  require DIRETORIO_FUNCTIONS . 'schedule/address/insercoes_endereco.php';
+
+  $db = abre_conexao();
+
+  # chamando função que verifica se a cidade já esta cadastrada na tabela de cidades
+  $idLocalidade = verificaLocalidadeCadastrada($db, $endereco['uf'], $endereco['localidade']);
+
+  # verificando se a cidade ainda não está cadastrada na tabela de cidades
+  if ((! isset($idLocalidade))) {
+    # chamando função que insere um registro na tabela de cidades
+    insereNovaLocalidade($db, $endereco['uf'], $endereco['localidade']);
+
+    # chamando novamente a função para recuperar o id da cidade inserido
+    $idLocalidade = verificaLocalidadeCadastrada($db, $endereco['uf'], $endereco['localidade']);
+  }
+
+  # chamando função que verifica se o bairro já está cadastrado na tabela de bairros
+  $idDistrito = verificaDistritoCadastrado($db, $idLocalidade, $endereco['distrito']);
+
+  # verificando se o bairro ainda não está cadastrado na tabela de bairros
+  if ((! isset($idDistrito))) {
+    # chamando função que insere um registro na tabela de bairros
+    insereNovoDistrito($db, $idLocalidade, $endereco['distrito']);
+
+    # chamando novamente a função para recuperar o id do bairro inserido
+    $idDistrito = verificaDistritoCadastrado($db, $idLocalidade, $endereco['distrito']);
+  }
+
+  # chamando função que insere um registro na tabela de endereços
+  atualizaEnderecoExistente($db, $endereco, $idDistrito);
+
+  $_SESSION['atividades']['tipo'] = 'success';
+  $_SESSION['atividades']['exibe'] = true;
+  $_SESSION['atividades']['mensagens'][] = 'Endereço editado com sucesso. Feche essa aba e na página de Atendimento selecione a Empresa novamente.';
+
+  header('Location:' . BASE_URL . 'public/views/schedule/address/edita_endereco.php?id=' . $endereco['id_cnpj']); exit;
+}
+
+/**
  * responsável por solicitar a inserção dos dados do endereço nas tabelas de estados, cidades, bairros e endereços
- * @param - array com o dados do endereço
+ * @param - array com os dados do endereço
  */
 function recebeNovoEndereco($endereco)
 {
@@ -40,10 +86,9 @@ function recebeNovoEndereco($endereco)
 
   $_SESSION['atividades']['tipo'] = 'success';
   $_SESSION['atividades']['exibe'] = true;
-  $_SESSION['atividades']['mensagens'][] = 'Endereço cadastrado com sucesso. Feche essa aba e na página de Atendimento selectione a Empresa novamente.';
+  $_SESSION['atividades']['mensagens'][] = 'Endereço cadastrado com sucesso. Feche essa aba e na página de Atendimento selecione a Empresa novamente.';
 
-  header('Location:' . BASE_URL . 'public/views/schedule/address/endereco.php?id=' . $endereco['id']);
-  exit;
+  header('Location:' . BASE_URL . 'public/views/schedule/address/endereco.php?id=' . $endereco['id']); exit;
 }
 
 /**
