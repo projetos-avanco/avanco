43c5ede934365c3dec9cf409ebc4dafeaefad520
Wellington Felix <wellington_bhmg@hotmail.com>
2018-10-30 15:28:02 -0300

    adicionando script que consulta os dados de uma empresa.

---
+++ capa/database/functions/schedule/remote/consultas_remoto.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * consulta o cnpj de uma empresa
+ * @param - objeto com uma conexão aberta
+ * @param - string com o id do cnpj da empresa
+ */
+function consultaCnpjDaEmpresa($db, $id)
+{
+  $query = "SELECT cnpj FROM av_agenda_cnpjs WHERE id = $id";
+
+  $resultado = mysqli_query($db, $query);
+
+  $cnpj = mysqli_fetch_row($resultado);
+  
+  return $cnpj[0];
+}
+
+/**
+ * consulta o nome da razão social de uma empresa
+ * @param - objeto com uma conexão aberta
+ * @param - string com o id do cnpj da empresa
+ */
+function consultaRazaoSocialDaEmpresa($db, $id)
+{
+  $query = "SELECT razao_social FROM av_agenda_cnpjs WHERE id = $id";
+
+  $resultado = mysqli_query($db, $query);
+
+  $razaoSocial = mysqli_fetch_row($resultado);
+  
+  return mb_strtoupper($razaoSocial[0]);
+}
+
+/**
+ * @param - objeto com uma conexão aberta
+ * @param - string com o id do cnpj da empresa
+ */
+function consultaContratoDaEmpresa($db, $id)
+{
+  $query =
+    "SELECT
+      t.conta_contrato
+    FROM av_agenda_cnpjs AS c
+    INNER JOIN av_agenda_contratos AS t
+      ON t.id = c.id_contrato
+    WHERE c.id = $id";
+  
+  $resultado = mysqli_query($db, $query);
+
+  $contrato = mysqli_fetch_row($resultado);
+  
+  return $contrato[0];
+}
