c164fdea1bf91acc6e0658c5c9788095691af44e
Wellington Felix <wellington_bhmg@hotmail.com>
2018-10-30 15:29:50 -0300

    alterando layout e apagando sessão de ticket.

--- capa/public/views/schedule/atendimento_remoto.php
+++ capa/public/views/schedule/atendimento_remoto.php
@@ -9,7 +9,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Portal Avanção - Cadastro de Atendimento Remoto</title>
+  <title>Portal Avanção - Cadastro Atendimento Remoto</title>
 
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -49,7 +49,7 @@
         <div class="row">
           <div class="col-sm-12">
             <div class="form-group">
-              <h2>Cadastro de Atendimento Remoto</h2>
+              <h2>Cadastro Atendimento Remoto</h2>
               <hr>
             </div>
           </div>
@@ -85,9 +85,9 @@
 
                     <div class="panel-body"><!-- panel-body -->
                       <div class="row">
-                        <div class="col-sm-4 col-sm-offset-8">
+                        <div class="col-sm-3 col-sm-offset-9">
                           <div class="form-group">
-                            <button class="btn btn-success btn-sm btn-block" id="nova-empresa" type="button">
+                            <button class="btn btn-info btn-sm btn-block" id="nova-empresa" type="button">
                               <i class="fa fa-building" aria-hidden="true"></i> Nova Empresa
                             </button>
                           </div>
@@ -209,7 +209,7 @@
                       <div class="row">
                         <div class="col-sm-3 col-sm-offset-9">
                           <div class="form-group">
-                            <button class="btn btn-success btn-sm btn-block" id="novo-contato" type="button">
+                            <button class="btn btn-info btn-sm btn-block" id="novo-contato" type="button">
                               <i class="fa fa-user-plus" aria-hidden="true"></i> Novo Contato
                             </button>
                           </div>
@@ -311,7 +311,7 @@
                   <div class="panel panel-info"><!-- panel -->
                     <div class="panel-heading">
                       <div class="text-center">
-                        <strong>Registro</strong>
+                        <strong>Ticket</strong>
                       </div>
                     </div>
 
@@ -380,7 +380,7 @@
 
       <?php endif; ?>
 
-      <?php unset($_SESSION['atividades']); ?>
+      <?php unset($_SESSION['atividades'], $_SESSION['ticket']); ?>
 
       </div><!-- container -->
     </div><!-- conteúdo da página -->
