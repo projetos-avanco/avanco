f5933e446efb04b36fbf45ca102a0b9851c0541f
Wellington Felix <wellington_bhmg@hotmail.com>
2018-10-30 15:28:49 -0300

    adicionando script que insere registros de atendimentos remotos.

---
+++ capa/database/functions/schedule/remote/insercoes_remoto.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * insere um atendimento remoto
+ * @param - objeto com uma conexão aberta
+ * @param - array com o dados de um atendimento remoto
+ */
+function insereAtendimentoRemoto($db, $remoto)
+{  
+  $query = 
+    "INSERT INTO 
+      av_agenda_atendimentos_remotos
+    VALUES (
+      {$remoto['id']},
+      {$remoto['id_cnpj']},
+      {$remoto['id_issue']},
+      {$remoto['registro']},
+     '{$remoto['tipo']}',
+      {$remoto['supervisor']},
+      {$remoto['colaborador']},
+     '{$remoto['status']}',
+     '{$remoto['data']}',
+     '{$remoto['horario']}',
+     '{$remoto['produto']}',
+     '{$remoto['modulo']}',
+     '{$remoto['observacao']}',
+      {$remoto['faturado']},
+      {$remoto['valor_hora']},
+      {$remoto['valor_pacote']},
+     '{$remoto['registrado']}'
+    )";
+
+  $resultado = mysqli_query($db, $query);
+  
+  return $resultado;
+}
