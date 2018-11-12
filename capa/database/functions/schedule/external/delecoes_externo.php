c6f3a1d1697ce9e5590d4dba3b6af477343c6571
Wellington Felix <wellington_bhmg@hotmail.com>
2018-11-12 16:52:50 -0200

    adicionando função que deleta uma pesquisa externa.

--- capa/database/functions/schedule/external/delecoes_externo.php
+++ capa/database/functions/schedule/external/delecoes_externo.php
@@ -1,6 +1,18 @@
 <?php
 
 /**
+ * deleta uma pesquisa externa
+ * @param - objeto com uma conexão aberta
+ * @param - inteiro com o id da pesquisa externa
+ */
+function deletaPesquisaExterna($db, $id)
+{
+  $query = "DELETE FROM av_agenda_pesquisas_externas WHERE id = $id";
+
+  mysqli_query($db, $query);
+}
+
+/**
  * deleta um registro de atendimento externo
  * @param - objeto com uma conexão aberta
  * @param - inteiro com o número do registro do antedimento externo
