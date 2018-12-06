49d02b53d9eb52593e70615608dbdb05d27ac466
Wellington Felix <wellington_bhmg@hotmail.com>
2018-12-06 16:40:47 -0200

    adicionando script de atualizações de atendimentos externos.

---
+++ capa/database/functions/schedule/external/atualizacoes_externo.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * confirma o status de um atendimento externo
+ * @param - objeto com uma conexão aberta
+ * @param - string com o id do atendimento externo
+ */
+function confirmaUmAtendimentoExterno($db, $id)
+{
+  $query = "UPDATE av_agenda_atendimentos_externos SET status = '2' WHERE id = $id";
+
+  $resultado = mysqli_query($db, $id);
+
+  return $resultado;
+}
+
+/**
+ * cancela o status de um atendimento externo
+ * @param - objeto com uma conexão aberta
+ * @param - string com o id do atendimento externo
+ */
+function cancelaUmAtendimentoExterno($db, $id)
+{
+  $query = "UPDATE av_agenda_atendimentos_externos SET status = '3' WHERE id = $id";
+
+  $resultado = mysqli_query($db, $id);
+
+  return $resultado;
+}
