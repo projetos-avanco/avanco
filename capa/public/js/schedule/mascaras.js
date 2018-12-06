3bf86c88da6883518d1881cce0f60823953d70a9
Wellington Felix <wellington_bhmg@hotmail.com>
2018-12-06 16:37:50 -0200

    adicionando máscaras.

--- capa/public/js/schedule/mascaras.js
+++ capa/public/js/schedule/mascaras.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  $('#data-inicial').mask('00/00/0000');
+  $('#data-final').mask('00/00/0000');
   $('#data').mask('00/00/0000');
   $('#horario').mask('00:00');
   $('#valor').mask('#0.00', {reverse: true});
