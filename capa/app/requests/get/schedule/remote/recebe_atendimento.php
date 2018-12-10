1a27cdaa43ff160ff3716282dba135602f4ea84a
Wellington Felix <wellington_bhmg@hotmail.com>
2018-12-10 11:23:47 -0200

    adicionando script que recebe o id de um atendimento remoto.

---
+++ capa/app/requests/get/schedule/remote/recebe_atendimento.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once '../../../../../init.php';
+require_once DIRETORIO_MODULES . 'schedule/modulo_remoto.php';
+
+# verificando se houve uma requisição via método get
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  # verificando se existe o id e se ele não está vazio
+  if (isset($_GET['id']) && !empty($_GET['id'])) {
+    # verificando se o id é uma string númerica
+    if (is_numeric($_GET['id'])) {
+      $id = (int) $_GET['id'];
+
+      # chamando função responsável por realizar o cancelamento do atendimento
+      solicitaCancelamentoDeAtendimento($id);
+    }
+  }
+}
