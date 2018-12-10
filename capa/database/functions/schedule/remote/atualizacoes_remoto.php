205bb6099acf3028f5ed9967efff889d2f41796e
Wellington Felix <wellington_bhmg@hotmail.com>
2018-12-10 11:24:34 -0200

    adicionando script de atualizações remotas.

---
+++ capa/database/functions/schedule/remote/atualizacoes_remoto.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * confirma o status de um atendimento remoto
+ * @param - objeto com uma conexão aberta
+ * @param - string com o id do atendimento remoto
+ */
+function confirmaUmAtendimentoRemoto($db, $id)
+{
+  $query = "UPDATE av_agenda_atendimentos_remotos SET status = '2' WHERE id = $id";
+
+  $resultado = mysqli_query($db, $id);
+
+  return $resultado;
+}
+
+/**
+ * cancela o status de um atendimento remoto
+ * @param - objeto com uma conexão aberta
+ * @param - string com o id do atendimento remoto
+ */
+function cancelaUmAtendimentoRemoto($db, $id)
+{
+  $query = "UPDATE av_agenda_atendimentos_remotos SET status = '3' WHERE id = $id";
+
+  mysqli_query($db, $query);
+}
