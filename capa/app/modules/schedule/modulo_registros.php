5ae96d97be53e3f8f8515db2e26525d3c32bcd84
Wellington Felix <wellington_bhmg@hotmail.com>
2018-12-13 14:13:43 -0200

    adicionando função de consultas de registros e a função que solicita as atualizações de registros.

--- capa/app/modules/schedule/modulo_registros.php
+++ capa/app/modules/schedule/modulo_registros.php
@@ -1,6 +1,142 @@
 <?php
 
 /**
+ * responsável por solicitar a atualização de um registro
+ * @param - array com os dados de um registro
+ * @param - string com o tipo do registro
+ */
+function solicitaAtualizacaoDeRegistro($dados, $tipo)
+{
+  require_once DIRETORIO_FUNCTIONS . 'schedule/records/atualizacoes_registros.php';  
+
+  $db = abre_conexao();
+
+  $_SESSION['atividades']['exibe'] = true;
+
+  switch ($tipo) {
+    case 'folgas':
+      $resultado = atualizaDadosDoRegistroDeFolga($db, $dados);
+
+      if (!$resultado) {
+        $_SESSION['atividades']['tipo'] = 'danger';
+        $_SESSION['atividades']['mensagens'][] = 'Não foi possível editar o registro. Informe ao Wellington Felix.';
+      } else {
+        $_SESSION['atividades']['tipo'] = 'success';
+        $_SESSION['atividades']['mensagens'][] = 'Registro editado com sucesso!';
+      }
+
+      header('location: ' . BASE_URL . 'public/views/schedule/edita_folgas.php?id=' . $dados['id']);
+        break;
+
+    case 'faltas':
+      $resultado = atualizaDadosDoRegistroDeFalta($db, $dados);
+
+      if (!$resultado) {
+        $_SESSION['atividades']['tipo'] = 'danger';
+        $_SESSION['atividades']['mensagens'][] = 'Não foi possível editar o registro. Informe ao Wellington Felix.';
+      } else {
+        $_SESSION['atividades']['tipo'] = 'success';
+        $_SESSION['atividades']['mensagens'][] = 'Registro editado com sucesso!';
+      }
+
+      header('location: ' . BASE_URL . 'public/views/schedule/edita_faltas.php?id=' . $dados['id']);
+        break;
+
+    case 'atrasos':
+      $resultado = atualizaDadosDoRegistroDeAtraso($db, $dados);
+
+      if (!$resultado) {
+        $_SESSION['atividades']['tipo'] = 'danger';
+        $_SESSION['atividades']['mensagens'][] = 'Não foi possível editar o registro. Informe ao Wellington Felix.';
+      } else {
+        $_SESSION['atividades']['tipo'] = 'success';
+        $_SESSION['atividades']['mensagens'][] = 'Registro editado com sucesso!';
+      }
+
+      header('location: ' . BASE_URL . 'public/views/schedule/edita_atrasos.php?id=' . $dados['id']);
+        break;
+
+    case 'extras':
+      $resultado = atualizaDadosDoRegistroDeExtra($db, $dados);
+
+      if (!$resultado) {
+        $_SESSION['atividades']['tipo'] = 'danger';
+        $_SESSION['atividades']['mensagens'][] = 'Não foi possível editar o registro. Informe ao Wellington Felix.';
+      } else {
+        $_SESSION['atividades']['tipo'] = 'success';
+        $_SESSION['atividades']['mensagens'][] = 'Registro editado com sucesso!';
+      }
+
+      header('location: ' . BASE_URL . 'public/views/schedule/edita_extras.php?id=' . $dados['id']);
+        break;
+  }  
+}
+
+/**
+ * responsável por consultar e retornar os dados do registro de folgas
+ * @param - inteiro com o id da folga
+ */
+function retornaDadosDoRegistroDeFolga($id)
+{
+  require_once DIRETORIO_FUNCTIONS . 'schedule/records/consultas_registros.php';
+
+  $db = abre_conexao();
+  
+  $dados = array();
+  $dados = consultaDadosDoRegistroDeFolga($db, $id);
+
+  return $dados;
+}
+
+/**
+ * responsável por consultar e retornar os dados do registro de faltas
+ * @param - inteiro com o id da falta
+ */
+function retornaDadosDoRegistroDeFalta($id)
+{
+  require_once DIRETORIO_FUNCTIONS . 'schedule/records/consultas_registros.php';
+
+  $db = abre_conexao();
+  
+  $dados = array();
+  $dados = consultaDadosDoRegistroDeFalta($db, $id);
+
+  return $dados;
+}
+
+/**
+ * responsável por consultar e retornar os dados do registro de atrasos
+ * @param - inteiro com o id da atraso
+ */
+function retornaDadosDoRegistroDeAtraso($id)
+{
+  require_once DIRETORIO_FUNCTIONS . 'schedule/records/consultas_registros.php';
+
+  $db = abre_conexao();
+  
+  $dados = array();
+  $dados = consultaDadosDoRegistroDeAtraso($db, $id);
+
+  return $dados;
+}
+
+/**
+ * responsável por consultar e retornar os dados do registro de extras
+ * @param - inteiro com o id da extra
+ */
+function retornaDadosDoRegistroDeExtra($id)
+{
+  require_once DIRETORIO_FUNCTIONS . 'schedule/records/consultas_registros.php';
+
+  $db = abre_conexao();
+  
+  $dados = array();
+  $dados = consultaDadosDoRegistroDeExtra($db, $id);
+
+  return $dados;
+}
+
+/**
  * responsável por solicitar a deleção de um registro de folgas
  * @param - inteiro com o id da folga
  */
