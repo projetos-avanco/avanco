774e03de50716c4713041278e5f6b4a452fc5baf
Wellington Felix <wellington_bhmg@hotmail.com>
2018-12-18 16:25:38 -0200

    adicionando botão de confirmação de visita.

--- capa/public/js/schedule/external/visualizar_atendimento_externo.js
+++ capa/public/js/schedule/external/visualizar_atendimento_externo.js
@@ -6,6 +6,8 @@ $(function() {
     var gerencial = {};
 
     gerencial.id                 = $(this).closest('tr').attr('data-id');
+    gerencial.id_contato         = $(this).closest('tr').attr('data-id-contato');
+    gerencial.id_cnpj            = $(this).closest('tr').attr('data-id-cnpj');
     gerencial.lancado            = $(this).closest('tr').attr('data-lancado');
     gerencial.registro           = $(this).closest('tr').attr('data-registro');
     gerencial.status             = $(this).closest('tr').attr('data-status');
@@ -24,28 +26,75 @@ $(function() {
     gerencial.pesquisa_realizada = $(this).closest('tr').attr('data-pesquisa');
 
     gerencial.empresa = gerencial.empresa.substr(0, 32).toUpperCase();
+    
+    if (gerencial.status === 'Pendente') {      
+      swal({
+        icon: 'info',
+        title: 'Atendimento Externo!',
+        text:               
+          'Lançado: '             + gerencial.lancado            + "\n\n" +
+          'Registro: '            + gerencial.registro           + "\n\n" +
+          'Situação: '            + gerencial.status             + "\n\n" +
+          'Supervisor: '          + gerencial.supervisor         + "\n\n" +
+          'Colaborador: '         + gerencial.colaborador        + "\n\n" +
+          'Tipo de Atendimento: ' + gerencial.tipo               + "\n\n\n\n" +
+          'Empresa: '             + gerencial.empresa            + "\n\n" +
+          'CNPJ: '                + gerencial.cnpj               + "\n\n" +
+          'Contato: '             + gerencial.contato            + "\n\n" +
+          'Período: '             + gerencial.periodo            + "\n\n" +
+          'Produto: '             + gerencial.produto            + "\n\n" +
+          'Observacao: '          + gerencial.observacao         + "\n\n\n\n" +
+          'Faturado: '            + gerencial.faturado           + "\n\n" +
+          'Despesas: '            + gerencial.despesas           + "\n\n" +
+          'Relatório Entregue: '  + gerencial.relatorio_entregue + "\n\n" +
+          'Pesquisa Realizada: '  + gerencial.pesquisa_realizada,
+        buttons: {
+          confirm: {
+            text: 'Confirmar Visita'
+          },
+          cancel: {
+            text: 'Fechar',
+            closeModal: true,
+            visible: true
+          }        
+        }
+      }).then((confirmar) => {        
+        var url = window.location.href;
+        var tmp = url.split('/');
 
-    // exibindo relatório completo pelo alert
-    swal({
-      icon: 'info',
-      title: 'Atendimento Externo!',
-      text:               
-        'Lançado: '             + gerencial.lancado            + "\n\n" +
-        'Registro: '            + gerencial.registro           + "\n\n" +
-        'Situação: '            + gerencial.status             + "\n\n" +
-        'Supervisor: '          + gerencial.supervisor         + "\n\n" +
-        'Colaborador: '         + gerencial.colaborador        + "\n\n" +
-        'Tipo de Atendimento: ' + gerencial.tipo               + "\n\n\n\n" +
-        'Empresa: '             + gerencial.empresa            + "\n\n" +
-        'CNPJ: '                + gerencial.cnpj               + "\n\n" +
-        'Contato: '             + gerencial.contato            + "\n\n" +
-        'Período: '             + gerencial.periodo            + "\n\n" +
-        'Produto: '             + gerencial.produto            + "\n\n" +
-        'Observacao: '          + gerencial.observacao         + "\n\n\n\n" +
-        'Faturado: '            + gerencial.faturado           + "\n\n" +
-        'Despesas: '            + gerencial.despesas           + "\n\n" +
-        'Relatório Entregue: '  + gerencial.relatorio_entregue + "\n\n" +
-        'Pesquisa Realizada: '  + gerencial.pesquisa_realizada
-    });
+        url = tmp[0]+'//'+tmp[2]+'/'+tmp[3]+'/'+tmp[4]+'/public/views/schedule/confirma_atendimento_externo.php?id-cnpj=' + gerencial.id_cnpj + '&id-contato=' + gerencial.id_contato;
+        
+        window.open(url, '_self');
+      });
+    } else {
+      swal({
+        icon: 'info',
+        title: 'Atendimento Externo!',
+        text:               
+          'Lançado: '             + gerencial.lancado            + "\n\n" +
+          'Registro: '            + gerencial.registro           + "\n\n" +
+          'Situação: '            + gerencial.status             + "\n\n" +
+          'Supervisor: '          + gerencial.supervisor         + "\n\n" +
+          'Colaborador: '         + gerencial.colaborador        + "\n\n" +
+          'Tipo de Atendimento: ' + gerencial.tipo               + "\n\n\n\n" +
+          'Empresa: '             + gerencial.empresa            + "\n\n" +
+          'CNPJ: '                + gerencial.cnpj               + "\n\n" +
+          'Contato: '             + gerencial.contato            + "\n\n" +
+          'Período: '             + gerencial.periodo            + "\n\n" +
+          'Produto: '             + gerencial.produto            + "\n\n" +
+          'Observacao: '          + gerencial.observacao         + "\n\n\n\n" +
+          'Faturado: '            + gerencial.faturado           + "\n\n" +
+          'Despesas: '            + gerencial.despesas           + "\n\n" +
+          'Relatório Entregue: '  + gerencial.relatorio_entregue + "\n\n" +
+          'Pesquisa Realizada: '  + gerencial.pesquisa_realizada,
+        buttons: {          
+          cancel: {
+            text: 'Fechar',
+            closeModal: true,
+            visible: true
+          }        
+        }
+      });
+    }
   });
 });
