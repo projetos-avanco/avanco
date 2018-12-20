5bf690e6005d715d2b837ffb5cc9af5d50630421
Wellington Felix <wellington_bhmg@hotmail.com>
2018-12-20 12:06:14 -0200

    modificando scripts para realizar o cancelamento de atendimentos remotos e externos.

--- capa/public/js/schedule/remote/cancelar_atendimento_remoto.js
+++ capa/public/js/schedule/remote/cancelar_atendimento_remoto.js
@@ -3,20 +3,80 @@ $(function() {
   $(document).on('click', '#cancelar-atendimento', function(e) {
     e.preventDefault;
 
-    var id = $(this).val();
-    var registro = $(this).closest('tr').attr('data-registro');
-    var url = window.location.href;
-    var tmp = url.split('/');
+    var id        = $(this).val();
+    var registro  = $(this).closest('tr').attr('data-registro');
+    var idCnpj    = $(this).closest('tr').attr('data-id-cnpj');
+    var idContato = $(this).closest('tr').attr('data-id-contato');
+    var status    = $(this).closest('tr').attr('data-status');
 
-    url = tmp[0]+'//'+tmp[2]+'/'+tmp[3]+'/'+tmp[4]+'/app/requests/get/schedule/remote/recebe_atendimento.php?id=' + id;
-
-    var resposta = confirm('Confirma a o cancelamento do atendimento de Registro - ' + registro + '?');
-
-    // verificando a resposta do usuário
-    if (resposta) {
-      window.open(url, '_self');
+    if (status === 'Confirmada') {
+      swal({
+        icon: 'info',
+        title: 'Atendimento Remoto!',
+        text: 'Confirma o Cancelamento do Atendimento de Registro: ' + registro + '?',
+        buttons: {
+          confirm: {
+            text: 'Confirmar Cancelamento'
+          },
+          cancel: {
+            text: 'Fechar',
+            closeModal: true,
+            visible: true
+          }        
+        }
+      }).then((confirmar) => {
+        if (confirmar) {
+          var url = window.location.href;
+          var tmp = url.split('/');
+  
+          url = tmp[0]+'//'+tmp[2]+'/'+tmp[3]+'/'+tmp[4]+'/public/views/schedule/cancela_atendimento_remoto.php?id=' + id + '&id-cnpj=' + idCnpj + '&id-contato=' + idContato;
+  
+          window.open(url, '_self');
+        }
+      });
     } else {
-      window.location.reload(true);
+      swal({
+        icon: 'info',
+        title: 'Atendimento Remoto!',
+        text: 'Confirma o Cancelamento do Atendimento de Registro: ' + registro + '?',
+        buttons: {
+          confirm: {
+            text: 'Confirmar Cancelamento'
+          },
+          cancel: {
+            text: 'Fechar',
+            closeModal: true,
+            visible: true
+          }        
+        }
+      }).then((confirmar) => {
+        if (confirmar) {
+          $.ajax({
+            type: 'get',
+            url: '../../../app/requests/get/schedule/remote/recebe_atendimento.php?id=' + id,
+            dataType: 'json',            
+            success: function(retorno) {
+              swal({
+                icon: 'success',
+                title: 'Atendimento Remoto',
+                text: 'Atendimento Remoto cancelado com sucesso.',
+                buttons: {                  
+                  cancel: {
+                    text: 'Fechar',
+                    closeModal: true,
+                    visible: true
+                  }        
+                }
+              }).then((confirmar) => {
+                location.reload();
+              });
+            },
+            error: function(erro) {
+              console.log(erro);
+            }
+          });          
+        }
+      });      
     }
   });
 });
