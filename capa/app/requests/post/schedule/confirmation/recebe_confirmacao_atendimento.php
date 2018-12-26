b6f7d22708c0dfce8343bb3dfa410edd937e6063
Wellington Felix <wellington_bhmg@hotmail.com>
2018-12-26 11:55:34 -0200

    validando envio de anexo das páginas de atendimento externo e remoto.

--- capa/app/requests/post/schedule/confirmation/recebe_confirmacao_atendimento.php
+++ capa/app/requests/post/schedule/confirmation/recebe_confirmacao_atendimento.php
@@ -5,11 +5,15 @@ require_once DIRETORIO_MODULES . 'schedule/modulo_confirmacao.php';
 
 # verificando se houve uma requisição via método post
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $copia = array();
+  $flag = false;
 
-  # verificando se o id do atendimento externo existe e não está vazio
+  $erros = array();
+
+  $copia = array();  
+
+  # verificando se o id do atendimento existe e não está vazio
   if (isset($_POST['id']) && (!empty($_POST['id']))) {
-    # verificando se o id do atendimento externo é uma string numérica
+    # verificando se o id do atendimento é uma string numérica
     if (is_numeric($_POST['id'])) {
       $id = (int) $_POST['id'];
     }
@@ -38,11 +42,51 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       array_push($copia, $_POST['copia'][$i]);
     }
   }
-
+  
   # verificando qual página realizou a requisição
   if (isset($_POST['pagina']) && $_POST['pagina'] === 'externo') {
-    solicitaEnvioDeEmailDeConfirmacaoExterno($id, $cnpj, $contato, $copia);
+    # verificando se foi enviado algum arquivo em anexo
+    if (isset($_FILES['externo']) && $_FILES['externo']['error']['anexo'] == 0) {          
+      # verificando se o tamanho do arquivo em anexo é maior que 2MB
+      if ($_FILES['externo']['size']['anexo'] > 2097152) {
+        $flag = true;
+        $erros['mensagens'][] = 'O arquivo em anexo deve ter o tamanho máximo de 2MB.';      
+      }
+    }
   } elseif (isset($_POST['pagina']) && $_POST['pagina'] === 'remoto') {
-    solicitaEnvioDeEmailDeConfirmacaoRemoto($id, $cnpj, $contato, $copia);
+    # verificando se foi enviado algum arquivo em anexo
+    if (isset($_FILES['remoto']) && $_FILES['remoto']['error']['anexo'] == 0) {          
+      # verificando se o tamanho do arquivo em anexo é maior que 2MB
+      if ($_FILES['remoto']['size']['anexo'] > 2097152) {
+        $flag = true;
+        $erros['mensagens'][] = 'O arquivo em anexo deve ter o tamanho máximo de 2MB.';      
+      }
+    }
   }
+
+  # abrindo sessão de validação
+  $_SESSION['atividades'] = array(
+    'tipo'      => 'danger',
+    'exibe'     => false,
+    'mensagens' => array()
+  );
+
+  if ($flag) {
+    $_SESSION['atividades']['exibe'] = true;
+
+    # repassando mensagens de erros para sessão
+    for ($i = 0; $i < count($erros); $i++) {
+      $_SESSION['atividades']['mensagens'][] = $erros[$i];
+    }
+
+    # redirecionando usuário para página de atendimento remoto
+    header('location:' . BASE_URL . 'public/views/schedule/confirma_atendimento_externo.php?id=' . $id . '&id-cnpj=' . $cnpj . '&id-contato=' . $contato); exit;
+  } else {
+    # verificando qual página realizou a requisição
+    if (isset($_POST['pagina']) && $_POST['pagina'] === 'externo') {
+      solicitaEnvioDeEmailDeConfirmacaoExterno($id, $cnpj, $contato, $copia);
+    } elseif (isset($_POST['pagina']) && $_POST['pagina'] === 'remoto') {
+      solicitaEnvioDeEmailDeConfirmacaoRemoto($id, $cnpj, $contato, $copia);
+    }
+  }  
 }
