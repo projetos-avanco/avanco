3a8767205c0fbd26e0b6005c3257799f5259a94e
Wellington Felix <wellington_bhmg@hotmail.com>
2018-12-27 16:48:56 -0200

    adicionando função que atualiza um atendimento externo.

--- capa/database/functions/schedule/external/atualizacoes_externo.php
+++ capa/database/functions/schedule/external/atualizacoes_externo.php
@@ -1,6 +1,38 @@
 <?php
 
 /**
+ * atualiza um atendimento externo
+ * @param - objeto com uma conexão aberta
+ * @param - array com os dados do atendimento externo
+ */
+function alteraAtendimentoExterno($db, $dados)
+{
+  $query =
+    "UPDATE
+      av_agenda_atendimentos_externos
+        SET
+          tipo         = '{$dados['tipo']}',
+          supervisor   =  {$dados['supervisor']},
+          colaborador  =  {$dados['colaborador']},
+          data_inicial = '{$dados['data_inicial']}',
+          data_final   = '{$dados['data_final']}',
+          horario      = '{$dados['horario']}',
+          produto      = '{$dados['produto']}',
+          modulo       = '{$dados['modulo']}',
+          observacao   = '{$dados['observacao']}',
+          faturado     =  {$dados['faturado']},
+          valor_hora   =  {$dados['valor_hora']},
+          valor_pacote =  {$dados['valor_pacote']},
+          despesa      =  {$dados['despesa']},
+          registrado   = '{$dados['registrado']}'
+    WHERE id = {$dados['id']}";
+  
+  $resultado = mysqli_query($db, $query);
+
+  return $resultado;
+}
+
+/**
  * confirma o status de um atendimento externo
  * @param - objeto com uma conexão aberta
  * @param - string com o id do atendimento externo
