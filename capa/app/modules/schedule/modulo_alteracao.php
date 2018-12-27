c67e10599a09453b66be8878809dfe2d7872309c
Wellington Felix <wellington_bhmg@hotmail.com>
2018-12-27 16:50:19 -0200

    adicionando módulo de alteração de atendimentos.

---
+++ capa/app/modules/schedule/modulo_alteracao.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * responsável por atualizar os dados de um atendimento externo e solicitar o envio do e-mail de alteração aos envolvidos
+ * @param - array com os dados do atendimento externo que devem ser alterados
+ * @param - array com os id's dos contatos que receberam o e-mail em cópia
+ */
+function recebeAlteracaoDeAtendimentoExterno($alterar, $copia)
+{
+  require_once DIRETORIO_HELPERS   . 'diversas.php';
+  require_once DIRETORIO_MODULES   . 'schedule/modulo_externo.php';
+  require_once DIRETORIO_FUNCTIONS . 'schedule/consultas_agenda.php';
+  require_once DIRETORIO_FUNCTIONS . 'schedule/external/atualizacoes_externo.php';
+  require_once DIRETORIO_FUNCTIONS . 'schedule/external/consultas_externo.php';
+  require_once DIRETORIO_FUNCTIONS . 'schedule/address/consultas_endereco.php';
+  require_once DIRETORIO_FUNCTIONS . 'schedule/contact/consultas_contato.php';
+
+  $db = abre_conexao();
+
+  $id = $alterar['id'];
+
+  $alteracoes = array(    
+    'colaborador'  => false,
+    'data_inicial' => false,
+    'data_final'   => false,    
+    'horario'      => false,
+    'observacao'   => false,
+    'faturado'     => false,
+    'despesa'      => false,
+    'valor_hora'   => false,
+    'valor_pacote' => false
+  );
+
+  # chamando função que retorna os dados de um atendimento externo
+  $externo = retornaDadosDoAtendimentoExterno($db, $id);
+
+  if ($alterar['colaborador']  != $externo['colaborador'])  {$alteracoes['colaborador']  = true;}
+  if ($alterar['data_inicial'] != $externo['data_inicial']) {$alteracoes['data_inicial'] = true;}
+  if ($alterar['data_final']   != $externo['data_final'])   {$alteracoes['data_final']   = true;}
+  if ($alterar['horario']      != $externo['horario'])      {$alteracoes['horario']      = true;}
+  if ($alterar['observacao']   != $externo['observacao'])   {$alteracoes['observacao']   = true;}
+  if ($alterar['faturado']     != $externo['faturado'])     {$alteracoes['faturado']     = true;}
+  if ($alterar['despesa']      != $externo['despesa'])      {$alteracoes['despesa']      = true;}
+  if ($alterar['valor_hora']   != $externo['valor_hora'])   {$alteracoes['valor_hora']   = true;}
+  if ($alterar['valor_pacote'] != $externo['valor_pacote']) {$alteracoes['valor_pacote'] = true;}
+
+  # verificando se os dados do atendimento externo foram alterados na tabela
+  if (alteraAtendimentoExterno($db, $alterar)) {
+    # chamando função que retorna os dados de um atendimento externo
+    $externo = retornaDadosDoAtendimentoExterno($db, $id);
+
+    $externo['cobranca'] = $alterar['cobranca'];
+
+    # chamando função que retorna o endereço do cliente
+    $endereco = retornaEnderecoAjax($db, $externo['id_cnpj']);
+
+    # chamando função que retorna os dados de um contato
+    $contato = consultaDadosDeUmContato($db, $externo['id_contato']);
+
+    # verificando se foram enviados os id's do contatos que receberam o e-mail em cópia
+    if (count($copia) > 0) {
+      for ($i = 0; $i < count($copia); $i++) {
+        $cc = consultaEnderecosEmailsDeUmContato($db, $copia[$i], $cc);
+      }
+    } else {
+      $cc['emails'] = array();
+    }
+
+    # verificando se será necessário enviar o e-mail de alteração aos envolvidos
+    if ($alterar['status'] == '1' || $alterar['status'] == '2') {
+      # chamando função que realiza o envio dos e-mails
+      if ($resultado = enviaEmailExterno($db, $externo, $endereco, $contato, $cc['emails'], 'alteracao', $alteracoes)) {
+        # e-mail foi enviado
+        $_SESSION['atividades']['tipo'] = 'success';
+        $_SESSION['atividades']['exibe'] = true;
+        $_SESSION['atividades']['mensagens'][] = 'Dados do atendimento externo alterados e E-mails enviados com sucesso.';
+      } else {
+        # e-mail não foi enviado
+        $_SESSION['atividades']['tipo'] = 'danger';
+        $_SESSION['atividades']['exibe'] = true;
+        $_SESSION['atividades']['mensagens'][] = 'Erro ao solicitar o envio de E-mail. Informe ao Wellington Felix.';
+        $_SESSION['atividades']['mensagens'][] = $resultado;
+      }
+    } else {
+      # dados do atendimento externo alterados
+      $_SESSION['atividades']['tipo'] = 'success';
+      $_SESSION['atividades']['exibe'] = true;
+      $_SESSION['atividades']['mensagens'][] = 'Dados do atendimento externo alterados com sucesso.';
+    }
+  } else {
+    $_SESSION['atividades']['tipo'] = 'danger';
+    $_SESSION['atividades']['exibe'] = true;
+    $_SESSION['atividades']['mensagens'][] = 'Erro ao alterar os dados do atendimento externo. Informe ao Wellington Felix.';
+  }
+  
+  # redirecionando usuário para página de atendimento remoto
+  header('location:' . BASE_URL . '/public/views/schedule/edita_atendimento_externo.php?id=' . $id); exit;
+}
