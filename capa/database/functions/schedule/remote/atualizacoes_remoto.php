8e33696f628cd734137e929856cabafde03e58a3
Wellington Felix <wellington_bhmg@hotmail.com>
2018-12-28 14:06:34 -0200

    adicionando função que altera so dados de um atendimento remoto.

--- capa/database/functions/schedule/remote/atualizacoes_remoto.php
+++ capa/database/functions/schedule/remote/atualizacoes_remoto.php
@@ -1,6 +1,36 @@
 <?php
 
 /**
+ * atualiza um atendimento remoto
+ * @param - objeto com uma conexão aberta
+ * @param - array com os dados do atendimento remoto
+ */
+function alteraAtendimentoRemoto($db, $dados)
+{
+  $query =
+    "UPDATE
+      av_agenda_atendimentos_remotos
+        SET
+          tipo         = '{$dados['tipo']}',
+          supervisor   =  {$dados['supervisor']},
+          colaborador  =  {$dados['colaborador']},
+          data         = '{$dados['data']}',          
+          horario      = '{$dados['horario']}',
+          produto      = '{$dados['produto']}',
+          modulo       = '{$dados['modulo']}',
+          observacao   = '{$dados['observacao']}',
+          faturado     =  {$dados['faturado']},
+          valor_hora   =  {$dados['valor_hora']},
+          valor_pacote =  {$dados['valor_pacote']},          
+          registrado   = '{$dados['registrado']}'
+    WHERE id = {$dados['id']}";
+  
+  $resultado = mysqli_query($db, $query);
+
+  return $resultado;
+}
+
+/**
  * confirma o status de um atendimento remoto
  * @param - objeto com uma conexão aberta
  * @param - string com o id do atendimento remoto
