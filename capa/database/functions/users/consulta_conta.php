8b617ab2d961b0f4f831366595b0ca7af281c298
Wellington Felix <wellington_bhmg@hotmail.com>
2018-12-28 08:52:58 -0200

    atualizando colaboradores do relatório de ramais.

--- capa/database/functions/users/consulta_conta.php
+++ capa/database/functions/users/consulta_conta.php
@@ -131,31 +131,28 @@ function retornaTodosOsRamaisDosUsuarios($db)
         case '5':
         case '6':
         case '7':
-        case '9':
-        case '10':
         case '11':
         case '13':
-        case '14':
-        case '15':
+        case '14':        
         case '16':
         case '17':
         case '18':
         case '19':
         case '21':
         case '22':
-        case '23':
-        case '24':
-        case '25':
-        case '26':
+        case '23':        
+        case '25':        
         case '27':
         case '28':
         case '29':
         case '30':
         case '33':
         case '34':
-        case '35':
-        case '36':
+        case '35':        
         case '37':
+        case '38':
+        case '39':
+        case '40':
 
           $arr['suporte'][] = array(
 
