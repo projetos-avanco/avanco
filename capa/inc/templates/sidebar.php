cef3c38d0817d2529167b97045fc259b75af0563
Wellington Felix <wellington_bhmg@hotmail.com>
2019-01-03 16:42:36 -0200

    adicionando opção de manifestação na barra lateral.

--- capa/inc/templates/sidebar.php
+++ capa/inc/templates/sidebar.php
@@ -177,6 +177,14 @@
               <p>Lançados<p>
             </a>
           </li>
+
+          <?php if ($_SESSION['usuario']['id'] == '5') : ?>
+          <li>
+            <a href="<?php echo BASE_URL; ?>../capa/public/views/vacation/exercicio_ferias_manifestacao.php">
+              <p>Manifestação<p>
+            </a>
+          </li>
+          <?php endif; ?>
         <?php elseif ($_SESSION['usuario']['nivel'] == '1') : ?>
           <li>
             <a href="<?php echo BASE_URL; ?>../capa/public/views/vacation/exercicio_ferias_pedidos.php">
