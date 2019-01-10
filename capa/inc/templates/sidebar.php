faed7b81f5ed9a61055967414fa713249f64f3f0
Wellington Felix <wellington_bhmg@hotmail.com>
2019-01-10 17:10:25 -0200

    permitindo visualização da página de exercícios lançados somente para adilson badaró.

--- capa/inc/templates/sidebar.php
+++ capa/inc/templates/sidebar.php
@@ -169,18 +169,18 @@
             <a href="<?php echo BASE_URL; ?>../capa/public/views/vacation/exercicio_ferias.php">
               <p>Exercício<p>
             </a>
-          </li>          
+          </li>
+
+          <li>
+            <a href="<?php echo BASE_URL; ?>../capa/public/views/vacation/exercicio_ferias_lancados.php">
+              <p>Lançados<p>
+            </a>
+          </li>
           <?php endif; ?>
 
           <li>
             <a href="<?php echo BASE_URL; ?>../capa/public/views/vacation/exercicio_ferias_pedidos.php">
               <p>Pedidos<p>
-            </a>
-          </li>
-          
-          <li>
-            <a href="<?php echo BASE_URL; ?>../capa/public/views/vacation/exercicio_ferias_lancados.php">
-              <p>Lançados<p>
             </a>
           </li>
 
