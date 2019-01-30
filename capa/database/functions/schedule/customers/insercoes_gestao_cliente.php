90d0735654fa76d88e2726ac26ca45d7eb60d8cf
Wellington Felix <wellington_bhmg@hotmail.com>
2019-01-29 23:29:08 -0200

    adicionando função que insere um atendimento que seja visita de relacionamento.

--- capa/database/functions/schedule/customers/insercoes_gestao_cliente.php
+++ capa/database/functions/schedule/customers/insercoes_gestao_cliente.php
@@ -1,6 +1,44 @@
 <?php
 
 /**
+ * insere um atendimento de gestão de clientes do tipo visita de cliente
+ * @param - objeto com uma conexão aberta
+ * @param - array com o dados de um atendimento de gestão de clientes
+ */
+function insereAtendimentoGestaoVisitaRelacionamento($db, $gestao)
+{  
+  $query = 
+    "INSERT INTO 
+      av_agenda_atendimentos_gestao_clientes
+    VALUES (
+      {$gestao['id']},
+      {$gestao['id_cnpj']},
+      {$gestao['id_issue']},
+      {$gestao['id_contato']},
+      {$gestao['registro']},
+     '{$gestao['tipo']}',
+      {$gestao['supervisor']},
+      {$gestao['colaborador']},
+     '{$gestao['status']}',
+     '{$gestao['data_inicial']}',
+     '{$gestao['data_final']}',
+     '{$gestao['horario']}',
+     '{$gestao['produto']}',
+     '{$gestao['modulo']}',
+     '{$gestao['observacao']}',
+      {$gestao['faturado']},
+      {$gestao['valor_hora']},
+      {$gestao['valor_pacote']},
+      {$gestao['despesa']},
+     '{$gestao['registrado']}'
+    )";
+
+  $resultado = mysqli_query($db, $query);
+  
+  return $resultado;
+}
+
+/**
  * insere um atendimento de gestão de clientes
  * @param - objeto com uma conexão aberta
  * @param - array com o dados de um atendimento de gestão de clientes
@@ -12,7 +50,8 @@ function insereAtendimentoGestao($db, $gestao)
       av_agenda_atendimentos_gestao_clientes
     VALUES (
       {$gestao['id']},
-      {$gestao['id_cnpj']},      
+      {$gestao['id_cnpj']},
+      null,
       {$gestao['id_contato']},
       {$gestao['registro']},
      '{$gestao['tipo']}',
