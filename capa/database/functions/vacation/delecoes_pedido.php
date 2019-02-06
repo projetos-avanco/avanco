b55610b83d7c2e082d0e47b0c546a8ab901e39d9
Wellington Felix <wellington_bhmg@hotmail.com>
2019-02-06 15:43:51 -0200

    adicionando nova função de deleção de pedidos.

--- capa/database/functions/vacation/delecoes_pedido.php
+++ capa/database/functions/vacation/delecoes_pedido.php
@@ -13,3 +13,18 @@ function deletaPedidos($db, $id)
 
   return $resultado;
 }
+
+/**
+ * deleta todos os pedidos de um exercício de férias
+ * @param - objeto com uma conexão aberta
+ * @param - inteiro com o id do exercício de férias
+ * @param - inteiro com o número do registro do pedido de férias
+ */
+function deletaTodosOsPedidos($db, $id, $registro)
+{
+  $query = "DELETE FROM av_agenda_pedidos_ferias WHERE (id_exercicio = $id) AND (registro = $registro)";
+
+  $resultado = mysqli_query($db, $query);
+
+  return $resultado;
+}
