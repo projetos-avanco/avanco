5c6c2e234f4186d1eaf69999cb267b5f8369a579
Wellington Felix <wellington_bhmg@hotmail.com>
2019-02-06 16:10:43 -0200

    adicionando função que retorna todos os exercícios de férias.

--- capa/app/modules/vacation/modulo_manifestacao.php
+++ capa/app/modules/vacation/modulo_manifestacao.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * responsável por retornar o exercício do ano vigente agendado do colaborador
+ * responsável por consultar e retornar o exercício do ano vigente agendado do colaborador
  * @param - inteiro com o id do colaborador 
  */
 function retornaExerciciosAgendadosDoColaborador($id)
@@ -13,4 +13,37 @@ function retornaExerciciosAgendadosDoColaborador($id)
   $tr = consultaExercicioAgendadoDoColaborador($db, $id);
 
   echo json_encode($tr); exit;
+}
+
+/**
+ * responsável por consultar e retornar todos os exercícios de férias informando se existe pedidos de férias aguardando aprovação
+ */
+function retornaTodosOsExerciciosDeFerias()
+{
+  require_once DIRETORIO_HELPERS   . 'datas.php';
+  require_once DIRETORIO_FUNCTIONS . 'vacation/consultas_manifestacao.php';
+
+  $db = abre_conexao();
+
+  # chamando função que consulta todos exercícios de férias
+  $dados = consultaTodosOsExerciciosDeFerias($db);
+
+  echo json_encode($dados);
+}
+
+/**
+ * responsável por consultar e retornar os pedidos de férias de um exercício
+ * @param - inteiro com o id do exercício de férias
+ */
+function retornaPedidosDeFeriasDeUmExercicio($id)
+{
+  require_once DIRETORIO_HELPERS   . 'datas.php';
+  require_once DIRETORIO_FUNCTIONS . 'vacation/consultas_manifestacao.php';
+
+  $db = abre_conexao();
+
+  # chamando função que consulta os pedidos de um exercício de férias
+  $pedidos = consultaPedidosDeFeriasDeUmExercicio($db, $id);
+
+  echo json_encode($pedidos);
 }
