dc9a3ec7b5c6622cbe8f9e0949912bb9df1a4865
Wellington Felix <wellington_bhmg@hotmail.com>
2019-02-06 15:46:22 -0200

    adicionando funções de consulta de exercícios e de pedidos de férias para a página de manifestação.

--- capa/database/functions/vacation/consultas_manifestacao.php
+++ capa/database/functions/vacation/consultas_manifestacao.php
@@ -60,3 +60,101 @@ function consultaExercicioAgendadoDoColaborador($db, $id)
 
   echo $tr; exit;
 }
+
+/**
+ * consulta todos os exercícios de férias informando se existem pedidos de férias aguardando aprovação
+ * @param - objeto com uma conexão aberta
+ */
+function consultaTodosOsExerciciosDeFerias($db)
+{
+  $query = 
+    "SELECT	
+      e.id,
+      e.colaborador AS id_colaborador,
+      CONCAT(s.name, ' ', s.surname) AS supervisor,
+      CASE
+        WHEN (e.status = true)
+          THEN 'Agendadas'
+        WHEN (e.status = false)
+          THEN 'Não Agendadas'
+      END AS status,
+      CONCAT(c.name, ' ', c.surname) AS colaborador,
+      e.exercicio_inicial AS exercicio_inicial,
+      e.exercicio_final AS exercicio_final,
+      e.vencimento AS vencimento,
+      DATE_FORMAT(e.registrado, '%Y-%m-%d') AS registrado
+    FROM av_agenda_exercicios_ferias AS e
+    INNER JOIN lh_users AS s
+      ON s.id = e.supervisor
+    INNER JOIN lh_users AS c
+      ON c.id = e.colaborador
+    WHERE (e.status = true)
+      ORDER BY e.exercicio_inicial DESC";
+  
+  $resultado = mysqli_query($db, $query);
+
+  $dados = array();
+
+  while ($linha = mysqli_fetch_array($resultado)) {
+    $linha['exercicio_inicial'] = formataDataParaExibir($linha['exercicio_inicial']);
+    $linha['exercicio_final']   = formataDataParaExibir($linha['exercicio_final']);
+    $linha['vencimento']        = formataDataParaExibir($linha['vencimento']);
+    $linha['registrado']        = formataDataParaExibir($linha['registrado']);
+
+    $dados[] = array(
+      'id'                => $linha['id'],
+      'id_colaborador'    => $linha['id_colaborador'],
+      'supervisor'        => $linha['supervisor'],
+      'status'            => $linha['status'],
+      'colaborador'       => $linha['colaborador'],
+      'exercicio_inicial' => $linha['exercicio_inicial'],
+      'exercicio_final'   => $linha['exercicio_final'],
+      'vencimento'        => $linha['vencimento'],
+      'registrado'        => $linha['registrado']
+    );
+  }
+
+  return $dados;
+}
+
+/**
+ * consulta os pedidos de um exercicio de férias
+ * @param - objeto com uma conexão aberta
+ * @param - inteiro com o id do exercício de férias
+ */
+function consultaPedidosDeFeriasDeUmExercicio($db, $id)
+{
+  $query =
+    "SELECT
+      e.registro,
+      CASE
+        WHEN (situacao = '1')
+          THEN 'Aguardando Aprovação'
+        WHEN (situacao = '2')
+          THEN 'Aprovado'
+      END AS situacao,
+      e.data_inicial,
+      e.data_final,
+      e.dias
+    FROM av_agenda_pedidos_ferias AS e
+    WHERE (e.id_exercicio = $id)";
+  
+  $resultado = mysqli_query($db, $query);
+
+  $pedidos = array();
+
+  while ($linha = mysqli_fetch_array($resultado)) {
+    $linha['data_inicial'] = formataDataParaExibir($linha['data_inicial']);
+    $linha['data_final']   = formataDataParaExibir($linha['data_final']);
+
+    $pedidos[] = array(
+      'registro'     => $linha['registro'],
+      'situacao'     => $linha['situacao'],
+      'data_inicial' => $linha['data_inicial'],
+      'data_final'   => $linha['data_final'],
+      'dias'         => $linha['dias']
+    );
+  }
+
+  return $pedidos;
+}
