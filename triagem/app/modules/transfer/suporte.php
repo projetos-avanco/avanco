be876dd2ca788dfbe43666d7dc99286b026c6bf3
Wellington Felix <wellington_bhmg@hotmail.com>
2019-12-08 19:56:50 -0300

    trabalhando na triagem para o novo portal.

--- triagem/app/modules/transfer/suporte.php
+++ triagem/app/modules/transfer/suporte.php
@@ -5,95 +5,98 @@
  */
 function consultaChatSuporte()
 {
-  $curva = false;
+    $curva = false;
 
-  # chamando função que retorna uma conexão com a base de dados
-  $conexao = abre_conexao();
+    # chamando função que retorna uma conexão com a base de dados
+    $conexao = abre_conexao();
 
-  # recuperando os dados do cliente que chamou no portal avanço
-  $cliente = $_SESSION['cliente'];
+    # recuperando os dados do cliente que chamou no portal avanço
+    $cliente = $_SESSION['cliente'];
 
-  # indicando que o cliente não utiliza o novo ERP
-  $cliente['novo_erp'] = '0';
-
-  $quantidade = 0;
-
-  # chamando função que retorna um array modelo para receber os dados dos colaboradores
-  $colaboradores = criaModeloDeColaboradores();
-
-  # chamando função que verifica se existe pelo menos um colaborador online no chat
-  $colaboradores = verificaColaboradoresOnlineNoChat($colaboradores, $conexao);
-
-  # recuperando a quantidade de colaboradores online
-  $quantidade = count($colaboradores);
-
-  ### === Curva ABC
-  if ($cliente['conta_contrato'] == '0000573' && $quantidade >= 1) {    
-    include 'customers/macdel.php';
-  }
-  ### === Curva ABC
-
-  # verificando se existe um ou mais colaboradores online
-  if ($quantidade == 1) {
-
-    # chamando função que monta uma URL e redireciona o cliente para o departamento que realizará o atendimento
-    redirecionaClienteParaDepartamento($colaboradores, $cliente, $curva);
-
-    # verificando se existe um ou mais colaboradores online no chat
-  } elseif ($quantidade > 1) {
-
-    # pesquisar conhecimento dos colaboradores
-    $colaboradores = verificaNivelDeConhecimentoDosColaboradoresOnline($colaboradores, $cliente, $quantidade, $conexao);
-
-    # chamando função que grava os colaboradores em uma sessão
-    criaSessaoDeColaboradores($colaboradores, $quantidade);
-
-    # chamando função que elimina os colaboradores que possuem menos de 20% de conhecimento
-    $colaboradores = eliminaColaboradoresSemConhecimento($colaboradores, $quantidade);
-
-    # recuperando a quantidade de colaboradores que possuem no mínimo 20% de conhecimento
-    $quantidade = count($colaboradores);
-
-    # verificando se não existe nenhum colaborador online com no mínimo 20% de conhecimento
-    if ($quantidade == 0) {
-
-      # recuperando dados dos colaboradores que estão online
-      $colaboradores = $_SESSION['colaboradores'];
-
-      # recuperando a quantidade de colaboradores online e com no mínimo 20% de conhecimento
-      $quantidade = count($colaboradores);
-
-      # chamando função que verifica a quantidade de fila
-      $colaboradores = verificaFilaDosColaboradores($colaboradores, $quantidade, $conexao);
-
-      # chamando função que ordena o array deixando o colaborador com menor fila na posição 0
-      usort($colaboradores, "comparaChavesDosArraysInternos");
-
-      # chamando função que monta uma URL e redireciona o cliente para o departamento que realizará o atendimento
-      redirecionaClienteParaDepartamento($colaboradores, $cliente, $curva);
-
-      # verificando se existe um ou mais colaboradores logados que possuem no mínimo 20% de conhecimento
-    } elseif ($quantidade > 0) {
-
-      # reordenando os valores do array começando por 0
-      $colaboradores = array_values($colaboradores);
-
-      # chamando função que verifica a quantidade de fila
-      $colaboradores = verificaFilaDosColaboradores($colaboradores, $quantidade, $conexao);
-
-      # chamando função que ordena o array deixando o colaborador com menor fila na posição 0
-      usort($colaboradores, "comparaChavesDosArraysInternos");
-
-      # chamando função que monta uma URL e redireciona o cliente para o departamento que realizará o atendimento
-      redirecionaClienteParaDepartamento($colaboradores, $cliente, $curva);
-
+    if ($cliente['produto'] == '39') {
+        # indicando que o cliente utiliza o novo ERP
+        $cliente['novo_erp'] = '1';
+    } else {
+        # indicando que o cliente não utiliza o novo ERP
+        $cliente['novo_erp'] = '0';
     }
 
-    # eliminando arrays
-    unset($colaboradores, $cliente);
+    $quantidade = 0;
 
-    # fechando conexão aberta
-    $conexao->close();
-  }
+    # chamando função que retorna um array modelo para receber os dados dos colaboradores
+    $colaboradores = criaModeloDeColaboradores();
 
+    # chamando função que verifica se existe pelo menos um colaborador online no chat
+    $colaboradores = verificaColaboradoresOnlineNoChat($colaboradores, $conexao);
+
+    # recuperando a quantidade de colaboradores online
+    $quantidade = count($colaboradores);
+
+    ### === Curva ABC
+    if ($cliente['conta_contrato'] == '0000573' && $quantidade >= 1) {
+        include 'customers/macdel.php';
+    }
+    ### === Curva ABC
+
+    # verificando se existe um ou mais colaboradores online
+    if ($quantidade == 1) {
+
+        # chamando função que monta uma URL e redireciona o cliente para o departamento que realizará o atendimento
+        redirecionaClienteParaDepartamento($colaboradores, $cliente, $curva);
+
+        # verificando se existe um ou mais colaboradores online no chat
+    } elseif ($quantidade > 1) {
+
+        # pesquisar conhecimento dos colaboradores
+        $colaboradores = verificaNivelDeConhecimentoDosColaboradoresOnline($colaboradores, $cliente, $quantidade, $conexao);
+
+        # chamando função que grava os colaboradores em uma sessão
+        criaSessaoDeColaboradores($colaboradores, $quantidade);
+
+        # chamando função que elimina os colaboradores que possuem menos de 20% de conhecimento
+        $colaboradores = eliminaColaboradoresSemConhecimento($colaboradores, $quantidade);
+
+        # recuperando a quantidade de colaboradores que possuem no mínimo 20% de conhecimento
+        $quantidade = count($colaboradores);
+
+        # verificando se não existe nenhum colaborador online com no mínimo 20% de conhecimento
+        if ($quantidade == 0) {
+
+            # recuperando dados dos colaboradores que estão online
+            $colaboradores = $_SESSION['colaboradores'];
+
+            # recuperando a quantidade de colaboradores online e com no mínimo 20% de conhecimento
+            $quantidade = count($colaboradores);
+
+            # chamando função que verifica a quantidade de fila
+            $colaboradores = verificaFilaDosColaboradores($colaboradores, $quantidade, $conexao);
+
+            # chamando função que ordena o array deixando o colaborador com menor fila na posição 0
+            usort($colaboradores, "comparaChavesDosArraysInternos");
+
+            # chamando função que monta uma URL e redireciona o cliente para o departamento que realizará o atendimento
+            redirecionaClienteParaDepartamento($colaboradores, $cliente, $curva);
+
+            # verificando se existe um ou mais colaboradores logados que possuem no mínimo 20% de conhecimento
+        } elseif ($quantidade > 0) {
+
+            # reordenando os valores do array começando por 0
+            $colaboradores = array_values($colaboradores);
+
+            # chamando função que verifica a quantidade de fila
+            $colaboradores = verificaFilaDosColaboradores($colaboradores, $quantidade, $conexao);
+
+            # chamando função que ordena o array deixando o colaborador com menor fila na posição 0
+            usort($colaboradores, "comparaChavesDosArraysInternos");
+
+            # chamando função que monta uma URL e redireciona o cliente para o departamento que realizará o atendimento
+            redirecionaClienteParaDepartamento($colaboradores, $cliente, $curva);
+        }
+
+        # eliminando arrays
+        unset($colaboradores, $cliente);
+
+        # fechando conexão aberta
+        $conexao->close();
+    }
 }
--- triagem/app/requests/post/recebe_cliente_portal.php
+++ triagem/app/requests/post/recebe_cliente_portal.php
@@ -3,76 +3,65 @@
 # verificando se foi enviado uma requisição via método POST pelo portal avanço ou Novo ERP
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-  require '../../../init.php';
+    require '../../../init.php';
 
-  require ABS_PATH . 'app/modules/transfer/tecnologia.php';
-  require ABS_PATH . 'app/modules/transfer/suporte.php';
-  require ABS_PATH . 'app/modules/transfer/novo_erp.php';
+    require ABS_PATH . 'app/modules/transfer/tecnologia.php';
+    require ABS_PATH . 'app/modules/transfer/suporte.php';
+    require ABS_PATH . 'app/modules/transfer/novo_erp.php';
 
-  # chamando função que verifica se os dados do cliente foram enviados
-  $confirma = confirmaEnvioDosDadosDoCliente();
+    # chamando função que verifica se os dados do cliente foram enviados
+    $confirma = confirmaEnvioDosDadosDoCliente();
 
-  # verificando se os dados do cliente foram enviados
-  if ($confirma) {
+    # verificando se os dados do cliente foram enviados
+    if ($confirma) {
 
-    # chamando função que retorna um array modelo para receber os dados do cliente
-    $cliente = criaModeloDeCliente();
+        # chamando função que retorna um array modelo para receber os dados do cliente
+        $cliente = criaModeloDeCliente();
 
-    # chamando função que recupera os dados do cliente no array super-global $_POST
-    recuperaDados($cliente, 'POST');
+        # chamando função que recupera os dados do cliente no array super-global $_POST
+        recuperaDados($cliente, 'POST');
 
-    # verificando se o cliente deseja falar no departamento do novo ERP
-    if (isset($_POST['cliente']['produto']) AND $_POST['cliente']['produto'] == '4') {
+        if (isset($_POST['cliente']['produto']) and $_POST['cliente']['produto'] == '5') {
 
-      # chamando função que consulta informações na base de dados do chat
-      consultaChatNovoErp();
+            # chamando função que consulta informações na base de dados do chat
+            consultaChatTecnologia();
 
-    # verificando se o cliente deseja falar no departamento de tecnologia
-    } elseif (isset($_POST['cliente']['produto']) AND $_POST['cliente']['produto'] == '5') {
+            # verificando se o cliente deseja falar no departamento de suporte geral
+        } else {
 
-      # chamando função que consulta informações na base de dados do chat
-      consultaChatTecnologia();
+            # chamando função que verifica se os dados da demanda foram enviados
+            $confirma = confirmaEnvioDosDadosDaDemanda();
 
-    # verificando se o cliente deseja falar no departamento de suporte geral
+            # verificando se os dados da demanda foram enviados
+            if ($confirma) {
+
+                # chamando função que consulta informações na base de dados do chat
+                consultaChatSuporte();
+            } else {
+
+                $msg = 'Produto, Módulo ou Dúvida não foram enviados!';
+
+                # retornando mensagem para o portal avanço
+                echo json_encode($msg, JSON_UNESCAPED_UNICODE);
+
+                exit;
+            }
+        }
     } else {
 
-      # chamando função que verifica se os dados da demanda foram enviados
-      $confirma = confirmaEnvioDosDadosDaDemanda();
-
-      # verificando se os dados da demanda foram enviados
-      if ($confirma) {
-
-        # chamando função que consulta informações na base de dados do chat
-        consultaChatSuporte();
-
-      } else {
-
-        $msg = 'Produto, Módulo ou Dúvida não foram enviados!';
+        $msg = 'Nome, Nome de Usuário, CNPJ, Conta Contrato ou Razão Social não foram enviados!';
 
         # retornando mensagem para o portal avanço
         echo json_encode($msg, JSON_UNESCAPED_UNICODE);
 
         exit;
-      }
-
     }
+} else {
 
-  } else {
-
-    $msg = 'Nome, Nome de Usuário, CNPJ, Conta Contrato ou Razão Social não foram enviados!';
+    $msg = 'Os dados do formulário não foram enviados via método POST!';
 
     # retornando mensagem para o portal avanço
     echo json_encode($msg, JSON_UNESCAPED_UNICODE);
 
     exit;
-  }
-
-} else {
-
-  $msg = 'Os dados do formulário não foram enviados via método POST!';
-
-  # retornando mensagem para o portal avanço
-  echo json_encode($msg, JSON_UNESCAPED_UNICODE);
-
-  exit;
 }
